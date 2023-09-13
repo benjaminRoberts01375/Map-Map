3728bb7bde078b2c4d3540dad967578961199cdc
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-09-13 19:33:51 -0400

    Show user location

--- Map Map/MapMapUIViewRep.swift
+++ Map Map/MapMapUIViewRep.swift
@@ -10,7 +10,19 @@ import SwiftUI
 
 extension MapMap: UIViewRepresentable {
     func makeUIView(context: Context) -> MKMapView {
-        var mapView = MKMapView()
+        let mapView = MKMapView()
+        
+        // Getting the user's location
+        userLocation.setupLocationManager()                         // Get permission from user to show on map
+        userLocation.onAuthorizationChanged = {                     // If authorization changed, try to get the user's location. If unable, use defaults.
+            region = MKCoordinateRegion(
+                center: userLocation.locationManager?.location?.coordinate ?? mapView.centerCoordinate,
+                latitudinalMeters: defaultRadius,
+                longitudinalMeters: defaultRadius
+            )                       // Get the user's region, and if unavailable, fallback to the current one
+            mapView.setRegion(region, animated: true)
+        }                // If user's preferences change, run this code to set map position accordingly
+        mapView.showsUserLocation = true
         
         return mapView
     }
