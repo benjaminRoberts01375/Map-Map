c58b493497929735e1778b40cb398fafa9bcd0b3
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-09-14 15:47:39 -0400

    Added top blur

--- Map Map/ContentView.swift
+++ Map Map/ContentView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    let blurAmount: CGFloat = 10
     var body: some View {
-        MapMap()
+        GeometryReader { geo in
+            ZStack {
+                MapMap()
+                VStack {
+                    BlurView()
+                        .frame(width: geo.size.width + blurAmount * 2, height: geo.safeAreaInsets.top + blurAmount * 2)
+                        .blur(radius: blurAmount)
+                        .offset(x: -blurAmount, y: -blurAmount * 3)
+                    Spacer()
+                } // Top blur
+                .allowsHitTesting(false)
+            }
             .ignoresSafeArea()
+        }
+        .background(Color.black)
     }
 }
 
