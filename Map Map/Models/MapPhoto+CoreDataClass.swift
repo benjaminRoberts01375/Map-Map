45db65755fb484f6a8e465f25052aed6b81ab464
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-06 17:06:23 -0400

    Created MapPhoto CoreData class

---
+++ Map Map/Models/MapPhoto+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  MapPhoto+CoreDataClass.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 10/6/23.
+//
+
+import Foundation
+import CoreData
+
+@objc(MapPhoto)
+public class MapPhoto: NSManagedObject {
+
+}
