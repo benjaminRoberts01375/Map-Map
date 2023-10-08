3260633850933fc950128c4b0c7a45502f73ebd8
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-08 17:46:34 -0400

    Init Core Data

---
+++ Map Map/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 10/8/23.
+//
+
+import CoreData
+import SwiftUI
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Map_Map")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data Error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
--- Map Map/Map_MapApp.swift
+++ Map Map/Map_MapApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct Map_MapApp: App {
+    @StateObject private var dataController = DataController()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
