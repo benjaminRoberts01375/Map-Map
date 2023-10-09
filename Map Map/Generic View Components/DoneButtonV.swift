9578f067b2a136c73c19d0e1d5f4aeb9e7007175
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-09 13:17:26 -0400

    Add generic Done button

---
+++ Map Map/Generic View Components/DoneButtonV.swift
@@ -0,0 +1,35 @@
+//
+//  DoneButtonV.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 10/9/23.
+//
+
+import SwiftUI
+
+struct DoneButton: View {
+    var enabled: Bool = true
+    var action: () -> Void = { }
+    
+    var body: some View {
+        Button(action: {
+            action()
+        }, label: {
+            Text("Done")
+                .padding(.horizontal, 20)
+                .padding(.vertical, 10)
+                .background(.blue.opacity(enabled ? 1 : 0.5))
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        })
+        .disabled(!enabled)
+    }
+}
+
+#Preview {
+    VStack {
+        Spacer()
+        DoneButton(enabled: false)
+        DoneButton(enabled: true)
+    }
+}
