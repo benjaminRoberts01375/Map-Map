ce6b6d449e06e64df3239160bf47d5c078f3a1ed
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-09 02:30:08 -0400

    Handle image creation in an init

--- Map Map/Models/MapPhoto+CoreDataClass.swift
+++ Map Map/Models/MapPhoto+CoreDataClass.swift
@@ -6,10 +6,33 @@
 //
 //
 
-import Foundation
 import CoreData
+import PhotosUI
+import SwiftUI
 
 @objc(MapPhoto)
 public class MapPhoto: NSManagedObject {
+    @Published var image: ImageStatus = .loading
+    
+    enum ImageStatus {
+        case loading
+        case success(Image)
+        case failure
+    }
+}
 
+extension MapPhoto {
+    convenience public init(rawPhoto: PhotosPickerItem?, insertInto context: NSManagedObjectContext) {
+        self.init(context: context)
+        Task {
+            if let data = try? await rawPhoto?.loadTransferable(type: Data.self) {
+                if let uiImage = UIImage(data: data) {
+                    self.image = .success(Image(uiImage: uiImage))
+                    self.objectWillChange.send()
+                    return
+                }
+            }
+            self.image = .failure
+        }
+    }
 }
