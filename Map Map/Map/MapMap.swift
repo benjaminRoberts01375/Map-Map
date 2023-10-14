ee80d4209911d639897340e989136c3ded407904
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-13 21:03:36 -0400

    Created gen1 SwiftUI map

--- Map Map/Map/MapMap.swift
+++ Map Map/Map/MapMap.swift
@@ -8,6 +8,27 @@
 import MapKit
 import SwiftUI
 
+struct MapMap: View {
+    @FetchRequest(sortDescriptors: []) var maps: FetchedResults<MapPhoto>
+    @State var cameraPos: MapCameraPosition = .userLocation(followsHeading: true, fallback: .automatic)
     
+    var body: some View {
+        Map(position: $cameraPos) {
+            ForEach(maps) { map in
+                if let coordinates = map.coordinates {
+                    Annotation(
+                        "Temp",
+                        coordinate: coordinates,
+                        anchor: .center,
+                        content: { AnyView(map.getMap(.fullImage)) }
+                    )
+                }
+            }
+        }
+        .mapStyle(.standard(elevation: .realistic))
     }
+}
+
+#Preview {
+    MapMap()
 }
