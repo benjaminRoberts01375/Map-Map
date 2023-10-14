220024000de035288f14605708d27500d5c0cd72
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-14 16:23:18 -0400

    Allow placing views on a map

--- Map Map/Map/MapMap.swift
+++ Map Map/Map/MapMap.swift
@@ -11,20 +11,29 @@ import SwiftUI
 struct MapMap: View {
     @FetchRequest(sortDescriptors: []) var maps: FetchedResults<MapPhoto>
     @State var cameraPos: MapCameraPosition = .userLocation(followsHeading: true, fallback: .automatic)
+    @State var scale: Double = 1
+    @State var rotation: Angle = .zero
     
     var body: some View {
         GeometryReader { geo in
             Map(position: $cameraPos) {
+                UserAnnotation()
                 ForEach(maps) { map in
-                    if let coordinates = map.coordinates {
-                        Annotation(
-                            "Temp",
-                            coordinate: coordinates,
-                            anchor: .center,
-                            content: { AnyView(map.getMap(.fullImage)) }
-                        )
+                    Annotation(
+                        "\(map.mapName ?? "")",
+                        coordinate: CLLocationCoordinate2D(latitude: 44.47301, longitude: -73.20390),
+                        anchor: .center
+                    )
+                    {
+                        AnyView(map.getMap(.fullImage))
+                            .frame(width: 50 * scale, height: 50 * scale)
+                            .rotationEffect(rotation)
                     }
                 }
+            }
+            .onMapCameraChange(frequency: .continuous) { update in
+                scale = 10000 / update.camera.distance
+                rotation = Angle(degrees: -update.camera.heading)
             }
             .safeAreaPadding([.top, .leading, .trailing])
             .mapStyle(.standard(elevation: .realistic))
