8e1776bde91d03b7489221e720b8bd75f348a958
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-13 23:53:36 -0400

    Gen1 SwiftUI Map

--- Map Map/Map/MapMap.swift
+++ Map Map/Map/MapMap.swift
@@ -13,19 +13,27 @@ struct MapMap: View {
     @State var cameraPos: MapCameraPosition = .userLocation(followsHeading: true, fallback: .automatic)
     
     var body: some View {
-        Map(position: $cameraPos) {
-            ForEach(maps) { map in
-                if let coordinates = map.coordinates {
-                    Annotation(
-                        "Temp",
-                        coordinate: coordinates,
-                        anchor: .center,
-                        content: { AnyView(map.getMap(.fullImage)) }
-                    )
+        GeometryReader { geo in
+            Map(position: $cameraPos) {
+                ForEach(maps) { map in
+                    if let coordinates = map.coordinates {
+                        Annotation(
+                            "Temp",
+                            coordinate: coordinates,
+                            anchor: .center,
+                            content: { AnyView(map.getMap(.fullImage)) }
+                        )
+                    }
                 }
             }
+            .safeAreaPadding([.top, .leading, .trailing])
+            .mapStyle(.standard(elevation: .realistic))
+            .mapControls {
+                MapUserLocationButton()
+                MapCompass()
+                MapScaleView(anchorEdge: .trailing)
+            }
         }
-        .mapStyle(.standard(elevation: .realistic))
     }
 }
 
