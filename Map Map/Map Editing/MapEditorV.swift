5cf821c50489621bbd936b7a16ff13ddbe7fc1e6
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-21 01:49:43 -0400

    Show name editing box

--- Map Map/Map Editing/MapEditorV.swift
+++ Map Map/Map Editing/MapEditorV.swift
@@ -13,13 +13,20 @@ struct MapEditor: View {
     @State var workingName: String = ""
     
     var body: some View {
-        VStack {
+        VStack(alignment: .center) {
             Spacer()
             AnyView(map.getMap(.fullImage))
             Spacer()
+            TextField("\(Image(systemName: "pencil")) Map name", text: $workingName)
+                .padding(.all, 5)
+                .background(.gray)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(radius: 10)
+                .frame(width: 205)
             HStack {
                 Button(action: {
                     map.isEditing = false
+                    map.mapName = workingName
                     try? moc.save()
                 }, label: {
                     Text("Done")
