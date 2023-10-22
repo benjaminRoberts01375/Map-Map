d925db9fd9095c43006fd7af2dd027042fc6e7c9
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-22 13:41:22 -0400

    Use a bottom drawer to show editor controls

--- Map Map/Map Editing/MapEditorV.swift
+++ Map Map/Map Editing/MapEditorV.swift
@@ -5,6 +5,7 @@
 //  Created by Ben Roberts on 10/9/23.
 //
 
+import Bottom_Drawer
 import SwiftUI
 
 struct MapEditor: View {
@@ -12,41 +13,53 @@ struct MapEditor: View {
     @EnvironmentObject var mapDetails: MapDetailsM
     @Environment(\.managedObjectContext) var moc
     @State var workingName: String = ""
+    @State var mapWidth: CGFloat = .zero
     
     var body: some View {
-        VStack(alignment: .center) {
-            Spacer()
-            AnyView(map.getMap(.fullImage))
-                .opacity(0.5)
-                .allowsHitTesting(false)
-            Spacer()
-            TextField("\(Image(systemName: "pencil")) Map name", text: $workingName)
-                .padding(.all, 5)
-                .background(.gray)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .shadow(radius: 10)
-                .frame(width: 205)
-            HStack {
-                Button(action: {
-                    map.coordinates = mapDetails.position
-                    map.rotation = NSDecimalNumber(value: mapDetails.rotation.degrees)
-                    map.scale = NSDecimalNumber(value: 1.0 / mapDetails.scale)
-                    map.mapName = workingName
-                    map.isEditing = false
-                    try? moc.save()
-                }, label: {
-                    Text("Done")
-                        .bigButton(backgroundColor: .blue)
-                })
-                Button(action: {
-                    moc.delete(map)
-                    try? moc.save()
-                }, label: {
-                    Text("Cancel")
-                        .bigButton(backgroundColor: .gray)
-                })
+        ZStack {
+            GeometryReader { geo in
+                AnyView(map.getMap(.fullImage))
+                    .frame(width: geo.size.width * 0.75, height: geo.size.height * 0.75)
+                    .opacity(0.5)
+                    .allowsHitTesting(false)
+                    .position(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).midY)
+                    .onChange(of: geo.size, initial: true) { _, update in
+                        mapWidth = update.width
+                    }
+            }
+            
+            BottomDrawer(verticalDetents: [.content], horizontalDetents: [.center]) {
+                VStack {
+                    TextField("\(Image(systemName: "pencil")) Map name", text: $workingName)
+                        .padding(.all, 5)
+                        .background(Color.gray.opacity(0.7))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .frame(width: 205)
+                    HStack {
+                        Button(action: {
+                            map.coordinates = mapDetails.position
+                            map.rotation = NSDecimalNumber(value: mapDetails.rotation.degrees)
+                            map.scale = NSDecimalNumber(value: mapWidth / mapDetails.scale)
+                            map.mapName = workingName
+                            map.isEditing = false
+                            try? moc.save()
+                        }) {
+                            Text("Done")
+                                .bigButton(backgroundColor: .blue)
+                        }
+                        Button(action: {
+                            moc.delete(map)
+                            try? moc.save()
+                        }) {
+                            Text("Cancel")
+                                .bigButton(backgroundColor: .gray)
+                        }
+                    }
+                    .padding(.bottom, 10)
+                }
             }
         }
-        .onAppear { workingName = map.mapName ?? "Unknown name" }
+        .onAppear { workingName = map.mapName ?? "Untitled Map" }
+        .ignoresSafeArea()
     }
 }
