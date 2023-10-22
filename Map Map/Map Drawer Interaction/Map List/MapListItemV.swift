a6e1b2e74b8c8b938393e746fde8ff67b74d101d
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-22 18:51:26 -0400

    Reorganized MapListItem

--- Map Map/Map Drawer Interaction/Map List/MapListItemV.swift
+++ Map Map/Map Drawer Interaction/Map List/MapListItemV.swift
@@ -12,35 +12,36 @@ struct MapListItem: View {
     @EnvironmentObject var mapDetails: MapDetailsM
     
     var body: some View {
-        HStack {
-            AnyView(photo.getMap(.thumbnail))
-                .clipShape(RoundedRectangle(cornerRadius: 5))
-                .frame(width: 100, height: 100)
-                .background(.thickMaterial)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .padding(.vertical)
-            VStack(alignment: .leading) {
-                Text(photo.mapName ?? "Unknown name")
-                    .padding(.vertical)
-                if let coordinates = photo.coordinates {
-                    VStack(alignment: .leading) {
-                        Text("Latitude: ")
-                        Text("\(coordinates.latitude.wholeDegrees)º \(coordinates.latitude.minutes)' \(coordinates.latitude.seconds)'' ")
-                            .fontWidth(.condensed)
-                            .onAppear {
-                                print(coordinates)
-                            }
-                        Text("Longitude: ")
-                        Text("\(coordinates.longitude.wholeDegrees)º \(coordinates.longitude.minutes)' \(coordinates.longitude.seconds)'' ")
-                            .fontWidth(.condensed)
+        VStack {
+            Text(photo.mapName ?? "Unknown name")
+                .font(.title3)
+                .padding(.vertical, 7)
+            HStack {
+                AnyView(photo.getMap(.thumbnail))
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                    .frame(width: 100, height: 100)
+                    .background(.thickMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                VStack(alignment: .leading) {
+                    Spacer()
+                    if let coordinates = photo.coordinates {
+                        VStack(alignment: .leading) {
+                            Text("Latitude: ")
+                            Text("\(coordinates.latitude.wholeDegrees)º \(coordinates.latitude.minutes)' \(coordinates.latitude.seconds)'' ")
+                                .fontWidth(.condensed)
+                                .padding(.bottom, 5)
+                            Text("Longitude: ")
+                            Text("\(coordinates.longitude.wholeDegrees)º \(coordinates.longitude.minutes)' \(coordinates.longitude.seconds)'' ")
+                                .fontWidth(.condensed)
+                        }
+                        .foregroundStyle(.secondary)
                     }
-                    .foregroundStyle(.secondary)
+                    else {
+                        Text("Unknown location")
+                    }
                 }
-                else {
-                    Text("Unknown location")
-                }
-                Spacer(minLength: 0)
             }
+            .padding(.bottom, 12)
         }
     }
 }
