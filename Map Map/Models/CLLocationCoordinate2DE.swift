d8b9d9f4d67b978be2fc793b68f72af21a0fd4d1
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-10-22 20:04:42 -0400

    Created easy zero CLLocationCoordinate2D

---
+++ Map Map/Models/CLLocationCoordinate2DE.swift
@@ -0,0 +1,12 @@
+//
+//  MKCoordinate2D.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 10/22/23.
+//
+
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static let zero = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+}
