9bc86eb41ec5c7b5bdb2f366c02d96cf0a10cb82
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-02 15:01:23 -0400

    Added drag gesture to handles

--- Map Map/Map Editing/HandleV.swift
+++ Map Map/Map Editing/HandleV.swift
@@ -12,10 +12,23 @@ struct HandleV: View {
     @State var previousFrameDragAmount: CGSize = .zero
     let handleSize: CGFloat = 30
     
+    var dragGesture: some Gesture {
+        DragGesture(coordinateSpace: .global)
+            .onChanged { update in
+                position.width += update.translation.width - previousFrameDragAmount.width
+                position.height += update.translation.height - previousFrameDragAmount.height
+                previousFrameDragAmount = update.translation
+            }
+            .onEnded { _ in
+                previousFrameDragAmount = .zero
+            }
+    }
+    
     var body: some View {
         Circle()
             .frame(width: handleSize, height: handleSize)
             .foregroundStyle(.white)
             .offset(position)
+            .gesture(dragGesture)
     }
 }
