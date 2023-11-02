5e3650804170eeefe59afa5de2686cf35bc90960
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-02 15:10:32 -0400

    Created extension for creating CGPoint from CGSize

---
+++ Map Map/Non-Map Map Extensions/CGPointE.swift
@@ -0,0 +1,14 @@
+//
+//  CGPointE.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 11/2/23.
+//
+
+import SwiftUI
+
+extension CGPoint {
+    init(size: CGSize) {
+        self.init(x: size.width, y: size.height)
+    }
+}
