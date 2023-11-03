6727454ad872e3502b65141abff79f17b21e9819
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-03 01:17:42 -0400

    Calculate grid subdivisions

--- Map Map/Map Editing/IrregularGridV.swift
+++ Map Map/Map Editing/IrregularGridV.swift
@@ -13,13 +13,40 @@ struct IrregularGridV: Shape {
     let bottomLeading: CGSize
     let bottomTrailing: CGSize
     
+    let gridLineCount: CGFloat = 2
+    
     func path(in rect: CGRect) -> Path {
+        let gridLineCount: CGFloat = self.gridLineCount + 1
         var path = Path()
-        path.move(to: CGPoint(size: topLeading))
-        path.addLine(to: CGPoint(size: topTrailing))
-        path.addLine(to: CGPoint(size: bottomTrailing))
-        path.addLine(to: CGPoint(size: bottomLeading))
-        path.addLine(to: CGPoint(size: topLeading))
+        for i in stride(from: 0, through: gridLineCount, by: 1) { // I'm sorry this is a lot of repeated code for now <3
+            // Horizontal lines
+            path.move(
+                to: CGPoint(
+                    x: (bottomLeading.width - topLeading.width) / gridLineCount * i + topLeading.width,
+                    y: (bottomLeading.height - topLeading.height) / gridLineCount * i + topLeading.height
+                )
+            )
+            path.addLine(
+                to: CGPoint(
+                    x: (bottomTrailing.width - topTrailing.width) / gridLineCount * i + topTrailing.width,
+                    y: (bottomTrailing.height - topTrailing.height) / gridLineCount * i + topTrailing.height
+                )
+            )
+            
+            // Vertical lines
+            path.move(
+                to: CGPoint(
+                    x: (topTrailing.width - topLeading.width) / gridLineCount * i + topLeading.width,
+                    y: (topTrailing.height - topLeading.height) / gridLineCount * i + topLeading.height
+                )
+            )
+            path.addLine(
+                to: CGPoint(
+                    x: (bottomTrailing.width - bottomLeading.width) / gridLineCount * i + bottomLeading.width,
+                    y: (bottomTrailing.height - bottomLeading.height) / gridLineCount * i + bottomLeading.height
+                )
+            )
+        }
         return path
     }
 }
