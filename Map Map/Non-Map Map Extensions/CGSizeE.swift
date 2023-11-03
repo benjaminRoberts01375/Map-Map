23a1e4fd11e18209f805796b10da487f075f7000
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-03 17:23:56 -0400

    Added / operator

--- Map Map/Non-Map Map Extensions/CGSizeE.swift
+++ Map Map/Non-Map Map Extensions/CGSizeE.swift
@@ -11,8 +11,13 @@ extension CGSize {
     static func * (size: CGSize, factor: CGFloat) -> CGSize {
         return CGSize(width: size.width * factor, height: size.height * factor)
     }
+    
+    static func / (size: CGSize, factor: CGFloat) -> CGSize {
+        return CGSize(width: size.width / factor, height: size.height / factor)
+    }
+    
     static func *= (size: inout CGSize, factor: CGFloat) {
             size.width *= factor
             size.height *= factor
-        }
+    }
 }
