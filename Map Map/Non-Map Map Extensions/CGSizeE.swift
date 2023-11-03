37e4bc33f8d9f9168a7ef8587fcffc004c47c01a
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-03 02:09:49 -0400

    Added extension for multiplying a CGSize

---
+++ Map Map/Non-Map Map Extensions/CGSizeE.swift
@@ -0,0 +1,14 @@
+//
+//  CGSizeE.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 11/3/23.
+//
+
+import Foundation
+
+extension CGSize {
+    static func * (size: CGSize, factor: CGFloat) -> CGSize {
+        return CGSize(width: size.width * factor, height: size.height * factor)
+    }
+}
