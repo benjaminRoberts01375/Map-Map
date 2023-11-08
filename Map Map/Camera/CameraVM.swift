84b240cc1f43d05800c288fd3605a9fd6ffa8cf4
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-08 15:52:45 -0500

    Created new view model for avoiding video stream

--- Map Map/Camera/CameraVM.swift
+++ Map Map/Camera/CameraVM.swift
@@ -48,7 +48,6 @@ final class CameraVM: NSObject, AVCapturePhotoCaptureDelegate, AVCaptureVideoDat
     
     func capturePhoto() {
         let settings = AVCapturePhotoSettings()
-        settings.photoQualityPrioritization = .quality
         photoOutput?.capturePhoto(with: settings, delegate: self)
     }
 
@@ -71,3 +70,57 @@ final class CameraVM: NSObject, AVCapturePhotoCaptureDelegate, AVCaptureVideoDat
     }
 }
 
+class CameraService {
+    var session: AVCaptureSession?
+    var delegate: AVCapturePhotoCaptureDelegate?
+
+    let output = AVCapturePhotoOutput()
+    let previewLayer = AVCaptureVideoPreviewLayer()
+
+    func start(delegate: AVCapturePhotoCaptureDelegate, completion: @escaping (Error?) -> ()) {
+        self.delegate = delegate
+        checkPermissions(completion: completion)
+    }
+
+    private func checkPermissions(completion: @escaping (Error?) -> ()) {
+            switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .notDetermined:
+                AVCaptureDevice.requestAccess(for: .video) { _ in
+                    Task { await self.setupCamera(completion: completion) }
+                }
+            case .authorized:
+                Task { await setupCamera(completion: completion) }
+            case .denied, .restricted:
+                break
+            @unknown default:
+                break
+            }
+    }
+
+    private func setupCamera(completion: @escaping (Error?) -> ()) async {
+        let session = AVCaptureSession()
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                if session.canAddInput(input) {
+                    session.addInput(input)
+                }
+                if session.canAddOutput(output) {
+                    session.addOutput(output)
+                }
+                previewLayer.videoGravity = .resizeAspectFill
+                previewLayer.session = session
+                session.startRunning()
+                self.session = session
+            } catch {
+                completion(error)
+            }
+        }
+    }
+
+    func capturePhoto() {
+        let settings = AVCapturePhotoSettings()
+        settings.photoQualityPrioritization = .quality
+        output.capturePhoto(with: settings, delegate: delegate!)
+    }
+}
