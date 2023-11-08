fbb4294a581459ba3cb10564841c9a8046bcd50f
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-07 20:44:28 -0500

    Created function for starting camera

--- Map Map/Camera/CameraV.swift
+++ Map Map/Camera/CameraV.swift
@@ -17,5 +17,31 @@ struct CameraView: View {
 
 @Observable
 final class CameraViewModel: NSObject, AVCapturePhotoCaptureDelegate {
+    private var captureSession = AVCaptureSession()
+
+    func startSession() {
+        if let camera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            do {
+                let cameraInput = try AVCaptureDeviceInput(device: camera)
+                if captureSession.canAddInput(cameraInput) {
+                    captureSession.addInput(cameraInput)
+                }
+                
+                photoOutput = AVCapturePhotoOutput()
+                if let photoOutput = photoOutput, captureSession.canAddOutput(photoOutput) {
+                    captureSession.addOutput(photoOutput)
+                }
+                
+                Task { captureSession.startRunning() }
+                
+                let videoOutput = AVCaptureVideoDataOutput()
+                videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+                captureSession.addOutput(videoOutput)
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
     }
 }
