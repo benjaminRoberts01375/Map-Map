b9b38d35e52fb517ca2dfba16987db17d95693ff
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-07 20:39:27 -0500

    Created a blank camera view

---
+++ Map Map/Camera/CameraV.swift
@@ -0,0 +1,15 @@
+//
+//  CameraV.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 11/7/23.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct CameraView: View {
+    var body: some View {
+        VStack {
+    }
+}
