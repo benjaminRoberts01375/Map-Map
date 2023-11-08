44694f74f98b358f2420b6f7f552477c27a1ff2b
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-08 15:54:26 -0500

    Create camera preview UIKit View

--- Map Map/Camera/CameraV.swift
+++ Map Map/Camera/CameraV.swift
@@ -5,6 +5,7 @@
 //  Created by Ben Roberts on 11/7/23.
 //
 
+import AVFoundation
 import SwiftUI
 
 struct CameraView: View {
@@ -35,6 +36,46 @@ struct CameraView: View {
         }
         .onDisappear {
             cameraVM.endSession()
+        }
+    }
+}
+struct CameraPreview: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIViewController
+    let cameraService: CameraService
+    let didFinishProcessingPhoto: (Result<AVCapturePhoto, Error>) -> ()
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        cameraService.start(delegate: context.coordinator) { error in
+            if let error = error { didFinishProcessingPhoto(.failure(error)) }
+        }
+        
+        let viewController = UIViewController()
+        viewController.view.layer.addSublayer(cameraService.previewLayer)
+        cameraService.previewLayer.frame = viewController.view.bounds // Fill the available area
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) { }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self, didFinishProcessingPhoto: didFinishProcessingPhoto)
+    }
+    
+    class Coordinator: NSObject, AVCapturePhotoCaptureDelegate {
+        let parent: CameraPreview
+        private let didFinishProcessingPhoto: (Result<AVCapturePhoto, Error>) -> ()
+        
+        init(parent: CameraPreview, didFinishProcessingPhoto: @escaping (Result<AVCapturePhoto, Error>) -> ()) {
+            self.parent = parent
+            self.didFinishProcessingPhoto = didFinishProcessingPhoto
+        }
+        
+        func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+            if let error = error {
+                didFinishProcessingPhoto(.failure(error))
+                return
+            }
+            didFinishProcessingPhoto(.success(photo))
         }
     }
 }
