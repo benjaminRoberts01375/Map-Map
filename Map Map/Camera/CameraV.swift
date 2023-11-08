c6698ed0319fd718db1b7f605244c28a327c6d4c
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-07 20:43:06 -0500

    Created class for working with the camera

--- Map Map/Camera/CameraV.swift
+++ Map Map/Camera/CameraV.swift
@@ -9,7 +9,13 @@ import SwiftUI
 import AVFoundation
 
 struct CameraView: View {
+    @State var cameraManager = CameraViewModel()
+
     var body: some View {
         VStack {
+}
+
+@Observable
+final class CameraViewModel: NSObject, AVCapturePhotoCaptureDelegate {
     }
 }
