0d31bb19e3d7f6ed45bb99ebe4e4c287d1fb5525
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-13 15:16:30 -0500

    Added caching for marginally improved performance

--- Map Map/MapMap Editing/Photo Editor/IrregularGridV.swift
+++ Map Map/MapMap Editing/Photo Editor/IrregularGridV.swift
@@ -8,42 +8,67 @@
 import SwiftUI
 
 struct IrregularGridV: Shape {
-    let topLeading: CGSize
-    let topTrailing: CGSize
-    let bottomLeading: CGSize
-    let bottomTrailing: CGSize
+    private let topLeading: CGSize
+    private let topTrailing: CGSize
+    private let bottomLeading: CGSize
+    private let leadingHorizontalProportion: CGPoint
+    private let trailingHorizontalProportion: CGPoint
+    private let leadingVerticalProportion: CGPoint
+    private let trailingVerticalProportion: CGPoint
     
-    let gridLineCount: CGFloat = 2
+    private let gridLineCount: CGFloat
+    
+    init(topLeading: CGSize, topTrailing: CGSize, bottomLeading: CGSize, bottomTrailing: CGSize, gridLineCount: CGFloat = 2) {
+        self.gridLineCount = gridLineCount + 1
+        self.leadingHorizontalProportion = CGPoint(
+            x: (bottomLeading.width - topLeading.width) / self.gridLineCount,
+            y: (bottomLeading.height - topLeading.height) / self.gridLineCount
+        )
+        self.trailingHorizontalProportion = CGPoint(
+            x: (bottomTrailing.width - topTrailing.width) / self.gridLineCount,
+            y: (bottomTrailing.height - topTrailing.height) / self.gridLineCount
+        )
+        self.leadingVerticalProportion = CGPoint(
+            x: (topTrailing.width - topLeading.width) / self.gridLineCount,
+            y: (topTrailing.height - topLeading.height) / self.gridLineCount
+        )
+        self.trailingVerticalProportion = CGPoint(
+            x: (bottomTrailing.width - bottomLeading.width) / self.gridLineCount,
+            y: (bottomTrailing.height - bottomLeading.height) / self.gridLineCount
+        )
+        self.topLeading = topLeading
+        self.topTrailing = topTrailing
+        self.bottomLeading = bottomLeading
+    }
     
     func path(in rect: CGRect) -> Path {
-        let gridLineCount: CGFloat = self.gridLineCount + 1
         var path = Path()
-        for i in stride(from: 0, through: gridLineCount, by: 1) { // I'm sorry this is a lot of repeated code for now <3
+        for i in stride(from: 0, through: gridLineCount, by: 1) {
             // Horizontal lines
             path.move(
                 to: CGPoint(
-                    x: (bottomLeading.width - topLeading.width) / gridLineCount * i + topLeading.width,
-                    y: (bottomLeading.height - topLeading.height) / gridLineCount * i + topLeading.height
+                    x: leadingHorizontalProportion.x * i + topLeading.width,
+                    y: leadingHorizontalProportion.y * i + topLeading.height
                 )
             )
             path.addLine(
                 to: CGPoint(
-                    x: (bottomTrailing.width - topTrailing.width) / gridLineCount * i + topTrailing.width,
-                    y: (bottomTrailing.height - topTrailing.height) / gridLineCount * i + topTrailing.height
+                    x: trailingHorizontalProportion.x * i + topTrailing.width,
+                    y: trailingHorizontalProportion.y * i + topTrailing.height
                 )
             )
             
             // Vertical lines
             path.move(
                 to: CGPoint(
-                    x: (topTrailing.width - topLeading.width) / gridLineCount * i + topLeading.width,
-                    y: (topTrailing.height - topLeading.height) / gridLineCount * i + topLeading.height
+                    x: leadingVerticalProportion.x * i + topLeading.width,
+                    y: leadingVerticalProportion.y * i + topLeading.height
                 )
             )
             path.addLine(
                 to: CGPoint(
-                    x: (bottomTrailing.width - bottomLeading.width) / gridLineCount * i + bottomLeading.width,
-                    y: (bottomTrailing.height - bottomLeading.height) / gridLineCount * i + bottomLeading.height
+                    x: trailingVerticalProportion.x * i + bottomLeading.width,
+                    y: trailingVerticalProportion.y * i + bottomLeading.height
                 )
             )
         }
