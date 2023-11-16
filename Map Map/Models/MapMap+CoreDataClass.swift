5adb4f3f76a74d223b170f0ad7b44f05afd3847e
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-16 16:33:29 -0500

    Removed old data safety check

--- Map Map/Models/MapMap+CoreDataClass.swift
+++ Map Map/Models/MapMap+CoreDataClass.swift
@@ -87,8 +87,6 @@ public class MapMap: NSManagedObject {
                         .resizable()
                         .scaledToFit()
                 )
-                if self.imageWidth == .zero { self.imageWidth = uiImage.size.width }
-                if self.imageHeight == .zero { self.imageHeight = uiImage.size.height }
                 if self.mapMapEncodedThumbnail == nil { generateThumbnailFromUIImage(uiImage) }
                 else { loadThumbnailFromCD() }
                 return
