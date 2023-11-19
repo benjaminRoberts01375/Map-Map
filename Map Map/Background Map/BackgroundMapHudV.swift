5cdfa1bfb55061e74edac7b9e51aa72d6de5939b
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-18 20:47:47 -0500

    Moved map hud into its own view

---
+++ Map Map/Background Map/BackgroundMapHudV.swift
@@ -0,0 +1,57 @@
+//
+//  MapCenterV.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 11/18/23.
+//
+
+import SwiftUI
+
+struct BackgroundMapHudV: View {
+    @EnvironmentObject var backgroundMapDetails: BackgroundMapDetailsM
+    let stringFormat: String = "%.4f"
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Latitude: ") +
+            Text("\(String(format: stringFormat, abs(backgroundMapDetails.position.latitude)))º ").fontWidth(.condensed) +
+            Text(backgroundMapDetails.position.latitude < 0 ? "S" : "N")
+            
+            Text("Longitude: ") +
+            Text("\(String(format: stringFormat, abs(backgroundMapDetails.position.longitude)))º ").fontWidth(.condensed) +
+            Text(backgroundMapDetails.position.longitude < 0 ? "W" : "E")
+            
+            Text("Heading: ") +
+            Text("\(String(format: stringFormat, backgroundMapDetails.userRotation.degrees))º ").fontWidth(.condensed) +
+            Text(determineHeadingLabel())
+        }
+        .padding(5)
+        .background(.regularMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 11))
+        .padding(.top, 25)
+        .padding(.leading)
+    }
+    
+    func determineHeadingLabel() -> String {
+        var label = ""
+        let shareOfThePie = 67.5
+        let quarter: Double = 90
+        if backgroundMapDetails.userRotation.degrees < shareOfThePie {
+            label += "N"
+        }
+        else if backgroundMapDetails.userRotation.degrees.isBetween(min: quarter * 4 - shareOfThePie, max: quarter * 4 + shareOfThePie) {
+            label += "N"
+        }
+        if backgroundMapDetails.userRotation.degrees.isBetween(min: quarter * 2 - shareOfThePie, max: quarter * 2 + shareOfThePie) {
+            label += "S"
+        }
+        if backgroundMapDetails.userRotation.degrees.isBetween(min: quarter - shareOfThePie, max: quarter + shareOfThePie) {
+            label += "E"
+        }
+        if backgroundMapDetails.userRotation.degrees.isBetween(min: quarter * 3 - shareOfThePie, max: quarter * 3 + shareOfThePie) {
+            label += "W"
+        }
+        
+        return label
+    }
+}
--- Map Map/Background Map/BackgroundMapLayersV.swift
+++ Map Map/Background Map/BackgroundMapLayersV.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct BackgroundMapLayersV: View {
-    @EnvironmentObject var backgroundMapDetails: BackgroundMapDetailsM
     let blurAmount: CGFloat = 10
-    let stringFormat: String = "%.4f"
     
     var body: some View {
         GeometryReader { geo in
@@ -22,50 +20,8 @@ struct BackgroundMapLayersV: View {
                     .blur(radius: blurAmount)
                     .ignoresSafeArea()
                     .allowsHitTesting(false)
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Latitude: ") + 
-                        Text("\(String(format: stringFormat, abs(backgroundMapDetails.position.latitude)))º ").fontWidth(.condensed) +
-                        Text(backgroundMapDetails.position.latitude < 0 ? "S" : "N")
-                        
-                        Text("Longitude: ") +
-                        Text("\(String(format: stringFormat, abs(backgroundMapDetails.position.longitude)))º ").fontWidth(.condensed) +
-                        Text(backgroundMapDetails.position.longitude < 0 ? "W" : "E")
-                        
-                        Text("Heading: ") +
-                        Text("\(String(format: stringFormat, backgroundMapDetails.userRotation.degrees))º ").fontWidth(.condensed) +
-                        Text(determineHeadingLabel())
-                    }
-                }
-                .padding(5)
-                .background(.regularMaterial)
-                .clipShape(RoundedRectangle(cornerRadius: 11))
-                .padding(.top, 25)
-                .padding(.leading)
+                BackgroundMapHudV()
             }
         }
-    }
-    
-    func determineHeadingLabel() -> String {
-        var label = ""
-        let shareOfThePie = 67.5
-        let quarter: Double = 90
-        if backgroundMapDetails.userRotation.degrees < shareOfThePie {
-            label += "N"
-        }
-        else if backgroundMapDetails.userRotation.degrees.isBetween(min: quarter * 4 - shareOfThePie, max: quarter * 4 + shareOfThePie) {
-            label += "N"
-        }
-        if backgroundMapDetails.userRotation.degrees.isBetween(min: quarter * 2 - shareOfThePie, max: quarter * 2 + shareOfThePie) {
-            label += "S"
-        }
-        if backgroundMapDetails.userRotation.degrees.isBetween(min: quarter - shareOfThePie, max: quarter + shareOfThePie) {
-            label += "E"
-        }
-        if backgroundMapDetails.userRotation.degrees.isBetween(min: quarter * 3 - shareOfThePie, max: quarter * 3 + shareOfThePie) {
-            label += "W"
-        }
-        
-        return label
     }
 }
