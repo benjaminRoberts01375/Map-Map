7e3e2820c19bb45ca795543a30445b710de2fb3b
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-24 22:24:17 -0500

    Added LocationDisplayMode enum

---
+++ Map Map/LocationDisplayMode.swift
@@ -0,0 +1,15 @@
+//
+//  LocationDisplayMode.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 11/24/23.
+//
+
+import MapKit
+import SwiftUI
+
+public enum LocationDisplayMode {
+    case degrees
+    case DMS
+}
+
