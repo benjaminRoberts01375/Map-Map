926f52fa26f837a1668389eece4fe820bd779ee3
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-28 15:19:46 -0500

    Created MarkerSymbolPickerItemV
    This may not be needed due to its simplicity

---
+++ Map Map/Marker Editor/MarkerSymbolPickerItemV.swift
@@ -0,0 +1,19 @@
+//
+//  MarkerSymbolPickerItemV.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 11/28/23.
+//
+
+import SwiftUI
+
+struct MarkerSymbolPickerItemV: View {
+    let symbol: String
+    
+    var body: some View {
+        Image(systemName: symbol)
+            .resizable()
+            .scaledToFit()
+            .foregroundStyle(.primary)
+    }
+}
