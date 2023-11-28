e6af14d7c759f8260a1a4118fee15ab56924c16f
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-28 14:14:08 -0500

    Added symbol picker view placeholder

---
+++ Map Map/Marker Editor/MarkerSymbolPickerV.swift
@@ -0,0 +1,20 @@
+//
+//  MarkerSymbolPickerV.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 11/28/23.
+//
+
+import SwiftUI
+
+struct MarkerSymbolPickerV: View {
+    let thumbnailImages = Mirror(reflecting: AvailableThumbnailImagesM()).children
+    
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    MarkerSymbolPickerV()
+}
