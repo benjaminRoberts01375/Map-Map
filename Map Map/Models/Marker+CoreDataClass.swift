800cae9f0f56c8c38accfc18cc1b6d2c729f0f17
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-28 20:53:40 -0500

    Added setter for backgroundColor

--- Map Map/Models/Marker+CoreDataClass.swift
+++ Map Map/Models/Marker+CoreDataClass.swift
@@ -49,14 +49,26 @@ public class Marker: NSManagedObject {
     }
     
     var backgroundColor: Color {
-        if let color = self.color {
-            return Color(red: color.red, green: color.green, blue: color.blue)
+        get {
+            if let color = self.color {
+                return Color(red: color.red, green: color.green, blue: color.blue)
+            }
+            else {
+                guard let context = self.managedObjectContext else { return Color.red }
+                let defaultColor = MarkerColor(red: 0.95, green: 0.30, blue: 0.30, insertInto: context)
+                self.color = defaultColor
+                return Color(red: defaultColor.red, green: defaultColor.green, blue: defaultColor.blue)
+            }
         }
-        else {
-            guard let context = self.managedObjectContext else { return Color.red }
-            let defaultColor = MarkerColor(red: 0.95, green: 0.30, blue: 0.30, insertInto: context)
-            self.color = defaultColor
-            return Color(red: defaultColor.red, green: defaultColor.green, blue: defaultColor.blue)
+        set(newValue) {
+            if let context = self.managedObjectContext {
+                var red: CGFloat = 0.0
+                var green: CGFloat = 0.0
+                var blue: CGFloat = 0.0
+                var alpha: CGFloat = 0.0
+                UIColor(newValue).getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+                self.color = MarkerColor(red: red, green: green, blue: blue, insertInto: context)
+            }
         }
     }
 }
