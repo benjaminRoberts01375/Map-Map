e09ea4a6117a7b8cfc41eed082dfd15aea0dfaf2
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-11-30 00:10:46 -0500

    Use Marker luminosity calculation to determine foreground color

--- Map Map/Marker Editor/MarkerSymbolPickerItemV.swift
+++ Map Map/Marker Editor/MarkerSymbolPickerItemV.swift
@@ -10,15 +10,23 @@ import SwiftUI
 struct MarkerSymbolPickerItemV: View {
     let symbol: String
     let backgroundColor: Color
+    let foregroundColor: Color
+    
+    init(symbol: String, backgroundColor: Color) {
+        self.symbol = symbol
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = Marker.calculateForgroundColor(color: backgroundColor)
+    }
     
     var body: some View {
         Circle()
             .foregroundStyle(backgroundColor)
+            .shadow(color: .primary.opacity(0.5), radius: 3)
             .overlay {
                 Image(systemName: symbol)
                     .resizable()
                     .scaledToFit()
-                    .foregroundStyle(.white)
+                    .foregroundStyle(foregroundColor)
                     .scaleEffect(0.6)
             }
     }
