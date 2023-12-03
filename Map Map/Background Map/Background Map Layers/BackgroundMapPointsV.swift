8e97f48bc62f068ad5fab5192f6bb80be5fb72cf
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-03 18:18:44 -0500

    Created proof of concept for detecting when over a MapMap

--- Map Map/Background Map/Background Map Layers/BackgroundMapPointsV.swift
+++ Map Map/Background Map/Background Map Layers/BackgroundMapPointsV.swift
@@ -11,9 +11,11 @@ import SwiftUI
 struct BackgroundMapPointsV: View {
     @EnvironmentObject var backgroundMapDetails: BackgroundMapDetailsM
     @FetchRequest(sortDescriptors: []) var markers: FetchedResults<Marker>
+    @FetchRequest(sortDescriptors: []) var mapMaps: FetchedResults<MapMap>
     @Environment(\.managedObjectContext) var moc
     @Binding var screenSpaceUserLocation: CGPoint?
     @Binding var screenSpaceMarkerLocations: [Marker : CGPoint]
+    @Binding var screenSpaceMapMapLocations: [MapMap : CGRect]
     let screenSize: CGSize
     static let iconSize: CGFloat = 30
     let userLocationSize: CGFloat = 24
@@ -50,6 +52,20 @@ struct BackgroundMapPointsV: View {
             }
         }
         
+        ForEach(mapMaps) { mapMap in
+            if let rect = screenSpaceMapMapLocations[mapMap] {
+                Rectangle()
+                    .fill(pointIntersectsRect(mapMap: mapMap) ? .red : .black)
+                    .frame(width: rect.width, height: rect.height)
+                    .rotationEffect(backgroundMapDetails.rotation - Angle(degrees: mapMap.mapMapRotation))
+                    .position(rect.origin)
+                    .opacity(0.5)
+                    .allowsHitTesting(false)
+                    .offset(y: -7)
+                    .ignoresSafeArea()
+            }
+        }
+        
         if let screenSpaceUserLocation = screenSpaceUserLocation {
             MapUserIcon()
                 .frame(width: userLocationSize, height: userLocationSize)
@@ -64,5 +80,34 @@ struct BackgroundMapPointsV: View {
         let yComponent = abs(markerPos.y - screenSize.height / 2)
         let distance = sqrt(pow(xComponent, 2) + pow(yComponent, 2))
         return distance < BackgroundMapPointsV.iconSize / 2
+    }
+    
+    func pointIntersectsRect(mapMap: MapMap) -> Bool {
+        guard let rect = screenSpaceMapMapLocations[mapMap] 
+        else { return false }
+        
+        let transform = CGAffineTransform(translationX: rect.midX - rect.width / 2, y: rect.midY - rect.height / 2)
+            .rotated(by: (backgroundMapDetails.rotation - Angle(degrees: mapMap.mapMapRotation)).radians)
+            .translatedBy(x: -rect.midX, y: -rect.midY)
+        
+        let offset: CGFloat = 50
+        
+        // Calculate rotated points
+        let rotatedPoints: [CGPoint] = [
+            CGPoint(x: rect.minX - offset, y: rect.minY - offset).applying(transform),
+            CGPoint(x: rect.maxX + offset, y: rect.minY - offset).applying(transform),
+            CGPoint(x: rect.maxX + offset, y: rect.maxY + offset).applying(transform),
+            CGPoint(x: rect.minX - offset, y: rect.maxY + offset).applying(transform)
+        ]
+        
+        // Check if the point is within the convex hull of rotated points
+        let convexHullPath = UIBezierPath()
+        convexHullPath.move(to: rotatedPoints[0])
+        convexHullPath.addLine(to: rotatedPoints[1])
+        convexHullPath.addLine(to: rotatedPoints[2])
+        convexHullPath.addLine(to: rotatedPoints[3])
+        convexHullPath.close()
+        
+        return convexHullPath.contains(CGPoint(size: screenSize / 2))
     }
 }
