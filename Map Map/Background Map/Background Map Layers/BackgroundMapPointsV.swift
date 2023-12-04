dcfe5737e80b3a3fd79c0dbb32b6a5f994a69235
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-04 15:11:59 -0500

    Remove mapmap highlighting

--- Map Map/Background Map/Background Map Layers/BackgroundMapPointsV.swift
+++ Map Map/Background Map/Background Map Layers/BackgroundMapPointsV.swift
@@ -54,20 +54,6 @@ struct BackgroundMapPointsV: View {
             }
         }
         
-        ForEach(mapMaps) { mapMap in
-            if let rect = screenSpaceMapMapLocations[mapMap] {
-                Rectangle()
-                    .fill(pointIntersectsRect(mapMap: mapMap) ? .red : .black)
-                    .frame(width: rect.width, height: rect.height)
-                    .rotationEffect(backgroundMapDetails.rotation - Angle(degrees: mapMap.mapMapRotation))
-                    .position(rect.origin)
-                    .opacity(0.5)
-                    .allowsHitTesting(false)
-                    .offset(y: -7)
-                    .ignoresSafeArea()
-            }
-        }
-        
         if let screenSpaceUserLocation = screenSpaceUserLocation {
             MapUserIcon()
                 .frame(width: userLocationSize, height: userLocationSize)
@@ -82,34 +68,5 @@ struct BackgroundMapPointsV: View {
         let yComponent = abs(markerPos.y - screenSize.height / 2)
         let distance = sqrt(pow(xComponent, 2) + pow(yComponent, 2))
         return distance < BackgroundMapPointsV.iconSize / 2
-    }
-    
-    func pointIntersectsRect(mapMap: MapMap) -> Bool {
-        guard let rect = screenSpaceMapMapLocations[mapMap] 
-        else { return false }
-        
-        let transform = CGAffineTransform(translationX: rect.midX - rect.width / 2, y: rect.midY - rect.height / 2)
-            .rotated(by: (backgroundMapDetails.rotation - Angle(degrees: mapMap.mapMapRotation)).radians)
-            .translatedBy(x: -rect.midX, y: -rect.midY)
-        
-        let offset: CGFloat = 50
-        
-        // Calculate rotated points
-        let rotatedPoints: [CGPoint] = [
-            CGPoint(x: rect.minX - offset, y: rect.minY - offset).applying(transform),
-            CGPoint(x: rect.maxX + offset, y: rect.minY - offset).applying(transform),
-            CGPoint(x: rect.maxX + offset, y: rect.maxY + offset).applying(transform),
-            CGPoint(x: rect.minX - offset, y: rect.maxY + offset).applying(transform)
-        ]
-        
-        // Check if the point is within the convex hull of rotated points
-        let convexHullPath = UIBezierPath()
-        convexHullPath.move(to: rotatedPoints[0])
-        convexHullPath.addLine(to: rotatedPoints[1])
-        convexHullPath.addLine(to: rotatedPoints[2])
-        convexHullPath.addLine(to: rotatedPoints[3])
-        convexHullPath.close()
-        
-        return convexHullPath.contains(CGPoint(size: screenSize / 2))
     }
 }
