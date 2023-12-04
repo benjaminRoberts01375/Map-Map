35149306070717a6174cde3c3034681ccbd1fc0b
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-04 13:02:13 -0500

    Allow comparing of CLLocationCoordinate2D's

--- Map Map/Non-Map Map Extensions/CLLocationCoordinate2DE.swift
+++ Map Map/Non-Map Map Extensions/CLLocationCoordinate2DE.swift
@@ -7,7 +7,11 @@
 
 import MapKit
 
-extension CLLocationCoordinate2D: Equatable {
+extension CLLocationCoordinate2D: Comparable, Equatable {
+    public static func < (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        return lhs.longitude + lhs.latitude < rhs.longitude + rhs.latitude
+    }
+    
     public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
         return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
     }
