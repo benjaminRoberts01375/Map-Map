bcf17276af2253fb44f6d24b6dcabd8cc3b84790
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-04 15:00:33 -0500

    Display the unsorted Markers

--- Map Map/MapMap List/MapMapsListV.swift
+++ Map Map/MapMap List/MapMapsListV.swift
@@ -15,25 +15,39 @@ struct MapMapList: View {
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        VStack(spacing: 0) {
-            ForEach(mapMaps) { mapMap in
-                Button(action: {
-                    withAnimation {
-                        backgroundMapDetails.mapCamera = .camera(MapCamera(centerCoordinate: mapMap.coordinates, distance: mapMap.mapDistance, heading: -mapMap.mapMapRotation))
-                    }
-                }, label: {
-                    MapMapListItemV(mapMap: mapMap)
-                        .padding()
-                })
-                .buttonStyle(.plain)
-                .background(colorScheme == .dark ? .gray20 : Color.white)
-                .contextMenu { MapMapContextMenuV(mapMap: mapMap) }
-                Divider()
-                CombineMarkerListItemsV(mapMap: mapMap)
+        VStack(alignment: .leading, spacing: 0) {
+            VStack(spacing: 0) {
+                ForEach(mapMaps) { mapMap in
+                    Button(action: {
+                        withAnimation {
+                            backgroundMapDetails.mapCamera = .camera(MapCamera(centerCoordinate: mapMap.coordinates, distance: mapMap.mapDistance, heading: -mapMap.mapMapRotation))
+                        }
+                    }, label: {
+                        MapMapListItemV(mapMap: mapMap)
+                            .padding()
+                    })
+                    .buttonStyle(.plain)
+                    .background(colorScheme == .dark ? .gray20 : Color.white)
+                    .contextMenu { MapMapContextMenuV(mapMap: mapMap) }
+                    Divider()
+                    CombineMarkerListItemsV(mapMap: mapMap)
+                }
             }
+            .background(colorScheme == .dark ? .gray20 : Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            
+            Text("Unsorted Markers")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.top)
+                .padding(.bottom, 5)
+            VStack {
+                MapMapListUnsortedMarkersV()
+            }
+            .padding(.top, 5)
+            .background(colorScheme == .dark ? .gray20 : Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
         }
         .ignoresSafeArea()
-        .background(colorScheme == .dark ? .gray20 : Color.white)
-        .clipShape(RoundedRectangle(cornerRadius: 15))
     }
 }
