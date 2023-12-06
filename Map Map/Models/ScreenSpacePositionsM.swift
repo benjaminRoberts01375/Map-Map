0fd22e7b9e77f24ebea3c3b621278227f80355bd
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-06 15:26:45 -0500

    Added function for generating rotated map map bounding boxes

--- Map Map/Models/ScreenSpacePositionsM.swift
+++ Map Map/Models/ScreenSpacePositionsM.swift
@@ -12,4 +12,33 @@ final class ScreenSpacePositionsM {
     public var markerPositions: [Marker : CGPoint] = [:]
     public var mapMapPositions: [MapMap : CGRect] = [:]
     public var userLocation: CGPoint? = nil
+    
+    func generateMapMapRotatedBounds(mapMap: MapMap, backgroundMapRotation: Angle) -> UIBezierPath? {
+        guard let rect: CGRect = mapMapPositions[mapMap]
+        else { return nil }
+        
+        let transform = CGAffineTransform(translationX: rect.midX - rect.width / 2, y: rect.midY - rect.height / 2)
+            .rotated(by: (backgroundMapRotation - Angle(degrees: mapMap.mapMapRotation)).radians)
+            .translatedBy(x: -rect.midX, y: -rect.midY)
+        
+        let offset: CGFloat = 50
+        
+        // Calculate rotated points
+        let rotatedPoints: [CGPoint] = [
+            CGPoint(x: rect.minX - offset, y: rect.minY - offset).applying(transform),
+            CGPoint(x: rect.maxX + offset, y: rect.minY - offset).applying(transform),
+            CGPoint(x: rect.maxX + offset, y: rect.maxY + offset).applying(transform),
+            CGPoint(x: rect.minX - offset, y: rect.maxY + offset).applying(transform)
+        ]
+        
+        // Check if the point is within the convex hull of rotated points
+        let convexHullPath = UIBezierPath()
+        convexHullPath.move(to: rotatedPoints[0])
+        convexHullPath.addLine(to: rotatedPoints[1])
+        convexHullPath.addLine(to: rotatedPoints[2])
+        convexHullPath.addLine(to: rotatedPoints[3])
+        convexHullPath.close()
+        
+        return convexHullPath
+    }
 }
