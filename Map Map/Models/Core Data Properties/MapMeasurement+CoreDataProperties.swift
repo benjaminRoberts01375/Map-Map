7e55f326b0a59d8bc4544f106f0e3baa58fe6d7b
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-15 11:49:15 -0500

    Created MapMeasurement CD entry

---
+++ Map Map/Models/Core Data Properties/MapMeasurement+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  MapMeasurement+CoreDataProperties.swift
+//  
+//
+//  Created by Ben Roberts on 12/15/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension MapMeasurement {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<MapMeasurement> {
+        return NSFetchRequest<MapMeasurement>(entityName: "MapMeasurement")
+    }
+
+    @NSManaged public var startingLongitude: Double
+    @NSManaged public var startingLatitude: NSDecimalNumber?
+    @NSManaged public var endingLongitude: Double
+    @NSManaged public var endingLatitude: Double
+    @NSManaged public var isEditing: Bool
+
+}
---
+++ Map Map/Models/MapMeasurement+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MapMeasurement+CoreDataClass.swift
+//  
+//
+//  Created by Ben Roberts on 12/15/23.
+//
+//
+
+import CoreData
+import Foundation
+
+@objc(MapMeasurement)
+public class MapMeasurement: NSManagedObject {
+
+}
