6fa2001197b92e5a3cb4003785b13f596227d5ce
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-15 11:57:03 -0500

    Added formatting for starting/ending coordinates

--- Map Map/Models/MapMeasurement+CoreDataClass.swift
+++ Map Map/Models/MapMeasurement+CoreDataClass.swift
@@ -8,8 +8,25 @@
 
 import CoreData
 import Foundation
+import MapKit
 
 @objc(MapMeasurement)
 public class MapMeasurement: NSManagedObject {
-
+    /// Format starting coordinates as CLLocationCoordinate2D.
+    var startingCoordinates: CLLocationCoordinate2D {
+        get { CLLocationCoordinate2D(latitude: self.startingLatitude, longitude: self.startingLongitude) }
+        set(newCoords) {
+            self.startingLatitude = newCoords.latitude
+            self.startingLongitude = newCoords.longitude
+        }
+    }
+    
+    /// Format ending coordinates as CLLocationCoordinate2D.
+    var endingCoordinates: CLLocationCoordinate2D {
+        get { CLLocationCoordinate2D(latitude: self.endingLatitude, longitude: self.endingLongitude) }
+        set(newCoords) {
+            self.endingLatitude = newCoords.latitude
+            self.endingLongitude = newCoords.longitude
+        }
+    }
 }
