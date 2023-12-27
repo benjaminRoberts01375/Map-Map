8a7834395ea8e80ad4c3f2ccfc0cf9482cb982e5
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-12-27 14:48:05 -0500

    Reworked handle rendering.
    Handles have gained several new features:
    - Settable color
    - Globally accessible default color
    - Position can be deferred to parent view.
    
    Handle styling has been moved to a file private view modifier.

--- Map Map/Generic View Components/HandleV.swift
+++ Map Map/Generic View Components/HandleV.swift
@@ -13,8 +13,12 @@ struct HandleV: View {
     @Binding var position: CGSize
     /// How far the handle was dragged up to the last frame.
     @State private var previousFrameDragAmount: CGSize = .zero
-    /// Size of the handle
-    private let handleSize: CGFloat = 30
+    /// Default color to make the handle
+    public static var defaultColor: Color = .white.opacity(0.5)
+    /// Actual color of the handle
+    public var color: Color = HandleV.defaultColor
+    /// Defer moving the handle to caller view.
+    public var deferPosition: Bool = false
     
     var dragGesture: some Gesture {
         DragGesture(coordinateSpace: .global)
@@ -29,11 +33,39 @@ struct HandleV: View {
     }
     
     var body: some View {
-        Circle()
-            .frame(width: handleSize, height: handleSize)
-            .foregroundStyle(.white.opacity(0.5))
-            .position(x: position.width, y: position.height)
-            .gesture(dragGesture)
+        if deferPosition {
+            Circle()
+                .handleAppearance(color: color)
+                .gesture(dragGesture)
+        }
+        else {
+            Circle()
+                .handleAppearance(color: color)
+                .gesture(dragGesture)
+                .position(x: position.width, y: position.height)
+        }
+    }
+}
+
+fileprivate struct HandleVModifier: ViewModifier {
+    /// Size of the handle.
+    public static let handleSize: CGFloat = 30
+    /// Handle padding distance.
+    public static let handlePadding: CGFloat = 10
+    /// Color of the handle.
+    public let color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: HandleVModifier.handleSize, height: HandleVModifier.handleSize)
+            .foregroundStyle(color)
             .shadow(radius: 2)
+            .padding(HandleVModifier.handlePadding)
+    }
+}
+
+extension View {
+    func handleAppearance(color: Color = .white) -> some View {
+        modifier(HandleVModifier(color: color))
     }
 }
