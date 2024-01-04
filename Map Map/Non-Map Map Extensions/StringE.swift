13709a231de0c6a5275876127dd8d12c66f25aab
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-01-04 11:27:16 -0500

    Added simple strings for Core Data notifications

---
+++ Map Map/Non-Map Map Extensions/StringE.swift
@@ -0,0 +1,15 @@
+//
+//  NSManagedObjectContextE.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 1/3/24.
+//
+
+import CoreData
+
+extension String {
+    public static var inserted: String = "inserted"
+    public static var deleted: String = "deleted"
+    public static var invalidatedAll: String = "invalidatedAll"
+    public static var update: String = "updated"
+}
