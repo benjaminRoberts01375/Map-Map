4c833e80428a35240895c41239c4e854082aa0f2
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-01-07 20:48:21 -0500

    Created onViewResizes

---
+++ Map Map/Generic View Components/ViewSizeVModifier.swift
@@ -0,0 +1,31 @@
+//
+//  ViewSizeVModifier.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 1/7/24.
+//
+
+import SwiftUI
+
+fileprivate struct OnViewResizes: ViewModifier {
+    @State var size: CGSize = .zero
+    let todo: (CGSize, CGSize) -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .background {
+                GeometryReader { imageGeo in
+                    Color.clear
+                        .onChange(of: imageGeo.size, initial: true) { initial, update in
+                            todo(initial, update)
+                        }
+                }
+            }
+    }
+}
+
+extension View {
+    func onViewResizes(_ todo: @escaping (CGSize, CGSize) -> Void) -> some View {
+        modifier(OnViewResizes(todo: todo))
+    }
+}
