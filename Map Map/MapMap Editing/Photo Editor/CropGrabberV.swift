9ffd9e597651d15877094d8dc14df901567e71de
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-01-16 15:38:42 -0500

    Moved the crop grabber's rotation into calculated value

--- Map Map/MapMap Editing/Photo Editor/CropGrabberV.swift
+++ Map Map/MapMap Editing/Photo Editor/CropGrabberV.swift
@@ -14,12 +14,21 @@ struct CropGrabberV: View {
     @Binding var trailingPoint: CGSize
     /// Length of the crop grabber
     static let length: CGFloat = 50
+    /// Angle of rotation for the Crop Grabber
+    var rotationAngle: Angle {
+        Angle(
+            radians: atan2(
+                leadingPoint.height - trailingPoint.height,
+                leadingPoint.width - trailingPoint.width
+            )
+        )
+    }
     
     var body: some View {
         RoundedRectangle(cornerRadius: 25.0)
             .foregroundStyle(.white)
             .frame(width: 60, height: 10)
-            .rotationEffect(Angle(radians: atan2(leadingPoint.height - trailingPoint.height, leadingPoint.width - trailingPoint.width)))
+            .rotationEffect(rotationAngle)
             .position(CGPoint(size: ((leadingPoint + trailingPoint) / 2)))
     }
 }
