4c0ad1d985838999a8fc2c5135d42f409715f493
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-01-17 15:24:59 -0500

    Added grabbers that are locked to axis

--- Map Map/MapMap Editing/Photo Editor/CropGrabberV.swift
+++ Map Map/MapMap Editing/Photo Editor/CropGrabberV.swift
@@ -28,11 +28,32 @@ struct CropGrabberV: View {
         CGPoint(size: ((leadingPoint + trailingPoint) / 2))
     }
     
+    func closestPointToPointOnVector(pointA: CGPoint, pointB: CGPoint, vectorV: CGVector) -> CGSize {
+        let vectorBA = CGVector(dx: pointB.x - pointA.x, dy: pointB.y - pointA.y) // Calculate the vector from A to B
+        let dotProduct = vectorBA.dx * vectorV.dx + vectorBA.dy * vectorV.dy // Calculate the dot product
+        let scalar = dotProduct / (pow(vectorV.dx, 2) + pow(vectorV.dy, 2)) // Calculate the scalar parameter
+        return CGSize(width: pointA.x + scalar * vectorV.dx, height: pointA.y + scalar * vectorV.dy) // Calculate the closest point
+    }
+    
+    var drag: some Gesture {
+        DragGesture(coordinateSpace: .local)
+            .onChanged { update in
+                let vec = CGVector(dx: cos(rotationAngle.radians + CGFloat.pi / 2), dy: sin(rotationAngle.radians + CGFloat.pi / 2))
+                let newPoint = closestPointToPointOnVector(pointA: position, pointB: update.location, vectorV: vec)
+                let delta = newPoint - CGSize(cgPoint: position)
+                leadingPoint += delta
+                trailingPoint += delta
+            }
+    }
+    
     var body: some View {
-        RoundedRectangle(cornerRadius: 25.0)
-            .foregroundStyle(.white)
-            .frame(width: 60, height: 10)
-            .rotationEffect(rotationAngle)
-            .position(position)
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0)
+                .foregroundStyle(.white)
+                .frame(width: 60, height: 10)
+                .rotationEffect(rotationAngle)
+                .position(position)
+                .gesture(drag)
+        }
     }
 }
