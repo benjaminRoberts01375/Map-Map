5f98db3e19d904578580ea6699e370c61b3ff246
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-01-20 16:15:30 -0500

    Added MarkupV

---
+++ Map Map/MapMap Editing/Markup Editor/MarkupV.swift
@@ -0,0 +1,21 @@
+//
+//  MarkupEditorV.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 1/20/24.
+//
+
+import PencilKit
+import SwiftUI
+
+struct DrawingView: UIViewRepresentable {
+    @Binding var canvasView: PKCanvasView
+    
+    func makeUIView(context: Context) -> PKCanvasView {
+        canvasView.tool = PKInkingTool(.pen, color: .black, width: 15)
+        canvasView.drawingPolicy = .anyInput
+        return canvasView
+    }
+    
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {}
+}
