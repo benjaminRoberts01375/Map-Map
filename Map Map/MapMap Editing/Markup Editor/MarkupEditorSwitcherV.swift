41de553c7e5fc8a23969ee1ac024c72e86b1b3bc
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-01-25 12:07:09 -0500

    Updated parameters for MarkupEditor

--- Map Map/MapMap Editing/Markup Editor/MarkupEditorSwitcherV.swift
+++ Map Map/MapMap Editing/Markup Editor/MarkupEditorSwitcherV.swift
@@ -15,52 +15,58 @@ struct MarkupEditorSwitcherV: View {
     @ObservedObject var mapMap: FetchedResults<MapMap>.Element
     @State private var canvasView: PKCanvasView = PKCanvasView()
     @State private var toolPicker = PKToolPicker()
+    @State private var mapMapSize: CGSize = .zero
     static let phoneDrawerHeight: CGFloat = 190
     private let isPhone: Bool
     
     init(mapMap: FetchedResults<MapMap>.Element) {
         self.mapMap = mapMap
         self.isPhone = UIDevice.current.userInterfaceIdiom == .phone
-        if let drawing = mapMap.drawing?.pkDrawing { canvasView.drawing = drawing }
+        if let drawing = mapMap.drawing {
+            if let pkDrawing = drawing.pkDrawing { canvasView.drawing = pkDrawing }
+            self.mapMapSize = CGSize(width: drawing.mapMapWidth, height: drawing.mapMapHeight)
+        }
     }
     
     var body: some View {
         ZStack {
-            if isPhone { MarkupEditorVIOS(mapMap: mapMap, canvasView: $canvasView) }
-            else { MarkupEditorVIPadOS(mapMap: mapMap, canvasView: $canvasView) }
+            if isPhone { MarkupEditorVIOS(mapMap: mapMap, canvasView: $canvasView, mapMapSize: $mapMapSize) }
+            else { MarkupEditorVIPadOS(mapMap: mapMap, canvasView: $canvasView, mapMapSize: $mapMapSize) }
             BottomDrawer(
                 verticalDetents: [self.isPhone ? .exactly(MarkupEditorSwitcherV.phoneDrawerHeight) : .content],
                 horizontalDetents: [.left, .right],
                 shortCardSize: 350
             ) { _ in
-                VStack {
-                    HStack {
-                        Button {
-                            if let drawing = mapMap.drawing { drawing.drawingData = canvasView.drawing.dataRepresentation() }
-                            else { _ = Drawing(context: moc, mapMap: mapMap, drawingData: canvasView.drawing.dataRepresentation()) }
-                            dismiss()
-                        } label: {
-                            Text("Done")
-                                .bigButton(backgroundColor: .blue)
-                        }
-                        Button {
-                            dismiss()
-                        } label: {
-                            Text("Cancel")
-                                .bigButton(backgroundColor: .gray)
-                        }
-                        Button {
-                            mapMap.drawing = nil
-                            dismiss()
-                        } label: {
-                            Text("Delete")
-                                .bigButton(backgroundColor: .red)
-                        }
+                HStack {
+                    Button {
+                        if let drawing = mapMap.drawing { drawing.drawingData = canvasView.drawing.dataRepresentation() }
+                        else { _ = Drawing(context: moc, mapMap: mapMap, drawingData: canvasView.drawing.dataRepresentation()) }
+                        dismiss()
+                    } label: {
+                        Text("Done")
+                            .bigButton(backgroundColor: .blue)
+                    }
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Cancel")
+                            .bigButton(backgroundColor: .gray)
+                    }
+                    Button {
+                        if let drawing = mapMap.drawing { moc.delete(drawing) }
+                        dismiss()
+                    } label: {
+                        Text("Delete")
+                            .bigButton(backgroundColor: .red)
                     }
                 }
             }
         }
         .onAppear { setupToolPicker() }
+        .onDisappear {
+            print(canvasView.bounds)
+            mapMap.drawing?.mapMapSize = mapMapSize
+        }
     }
     
     private func setupToolPicker() {
