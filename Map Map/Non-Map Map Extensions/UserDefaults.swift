76bff38868332341f5f5d69ad330227cb975a768
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-01-29 19:16:49 -0500

    Created extension to UserDefaults
    Extension holds related data, such as a default value, and key

---
+++ Map Map/Non-Map Map Extensions/UserDefaults.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaults.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 1/29/24.
+//
+
+import SwiftUI
+
+extension UserDefaults {
+    static let defaultCoordinateDisplayType: LocationDisplayMode = .degrees
+    static let coordinateDisplayType: String = "coordinateDisplayType"
+}
