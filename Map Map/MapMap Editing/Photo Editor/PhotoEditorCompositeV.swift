5017cb0e8a50d7aa6a657ff9206090aa6de9c070
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-02 14:20:31 -0500

    Keep automatic document detection in PhotoEditorV

--- Map Map/MapMap Editing/Photo Editor/PhotoEditorCompositeV.swift
+++ Map Map/MapMap Editing/Photo Editor/PhotoEditorCompositeV.swift
@@ -81,16 +81,6 @@ struct PhotoEditorCompositeV: View {
                 .padding(.bottom, isShortCard ? 0 : 10)
             }
         }
-        .task {
-            if mapMap.cropCorners == nil,
-               let mapMapImageData = mapMap.mapMapRawEncodedImage,
-               let ciImage = CIImage(data: mapMapImageData),
-               let generatedCorners = PhotoEditorCompositeV.detectDocumentCorners(image: ciImage, displaySize: screenSpaceImageSize) {
-                DispatchQueue.main.async {
-                    handleTracker = generatedCorners
-                }
-            }
-        }
     }
     
     /// Sets up and triggers the crop function for a Map Map.
@@ -117,43 +107,5 @@ struct PhotoEditorCompositeV: View {
         handleTracker.bottomLeading = CGSize(width: .zero, height: screenSpaceImageSize.height)
         handleTracker.bottomTrailing = screenSpaceImageSize
         mapMap.cropCorners = nil
-    }
-    
-    /// Detect where the corners of a rectangle in a photo are.
-    /// - Parameters:
-    ///   - imageData: Raw data from an image.
-    ///   - displaySize: Size of the photo currently being displayed.
-    /// - Returns: Positions of corners if found.
-    static func detectDocumentCorners(image: CIImage, displaySize: CGSize) -> FourCornersStorage? {
-        var corners: FourCornersStorage?
-        let rectangleDetectionRequest = VNDetectRectanglesRequest { request, _ in
-            guard let results = request.results as? [VNRectangleObservation], !results.isEmpty,
-                  let rectangle = results.first // Get first rectangle
-            else { return }
-            
-            // Scale coordinates from 0...1 to screen dimensions, and correct for upside down and mirror
-            corners = FourCornersStorage(
-                topLeading: CGSize(
-                    width: rectangle.topLeft.x * displaySize.width,
-                    height: displaySize.height - rectangle.topLeft.y * displaySize.height
-                ),
-                topTrailing: CGSize(
-                    width: rectangle.topRight.x * displaySize.width,
-                    height: displaySize.height - rectangle.topRight.y * displaySize.height
-                ),
-                bottomLeading: CGSize(
-                    width: rectangle.bottomLeft.x * displaySize.width,
-                    height: displaySize.height - rectangle.bottomLeft.y * displaySize.height
-                ),
-                bottomTrailing: CGSize(
-                    width: rectangle.bottomRight.x * displaySize.width,
-                    height: displaySize.height - rectangle.bottomRight.y * displaySize.height
-                )
-            )
-        }
-        
-        let handler = VNImageRequestHandler(ciImage: image, options: [:])
-        try? handler.perform([rectangleDetectionRequest])
-        return corners
     }
 }
--- Map Map/MapMap Editing/Photo Editor/PhotoEditorV.swift
+++ Map Map/MapMap Editing/Photo Editor/PhotoEditorV.swift
@@ -32,5 +32,54 @@ struct PhotoEditorV: View {
             .ignoresSafeArea()
         }
         .background(.black)
+        .task {
+            if mapMap.cropCorners == nil,
+               let mapMapImageData = mapMap.mapMapRawEncodedImage,
+               let ciImage = CIImage(data: mapMapImageData),
+               let generatedCorners = PhotoEditorV.detectDocumentCorners(image: ciImage, displaySize: screenSpaceImageSize) {
+                DispatchQueue.main.async {
+                    handleTracker = generatedCorners
+                }
+            }
+        }
+    }
+    
+    
+    /// Detect where the corners of a rectangle in a photo are.
+    /// - Parameters:
+    ///   - imageData: Raw data from an image.
+    ///   - displaySize: Size of the photo currently being displayed.
+    /// - Returns: Positions of corners if found.
+    static func detectDocumentCorners(image: CIImage, displaySize: CGSize) -> FourCornersStorage? {
+        var corners: FourCornersStorage?
+        let rectangleDetectionRequest = VNDetectRectanglesRequest { request, _ in
+            guard let results = request.results as? [VNRectangleObservation], !results.isEmpty,
+                  let rectangle = results.first // Get first rectangle
+            else { return }
+            
+            // Scale coordinates from 0...1 to screen dimensions, and correct for upside down and mirror
+            corners = FourCornersStorage(
+                topLeading: CGSize(
+                    width: rectangle.topLeft.x * displaySize.width,
+                    height: displaySize.height - rectangle.topLeft.y * displaySize.height
+                ),
+                topTrailing: CGSize(
+                    width: rectangle.topRight.x * displaySize.width,
+                    height: displaySize.height - rectangle.topRight.y * displaySize.height
+                ),
+                bottomLeading: CGSize(
+                    width: rectangle.bottomLeft.x * displaySize.width,
+                    height: displaySize.height - rectangle.bottomLeft.y * displaySize.height
+                ),
+                bottomTrailing: CGSize(
+                    width: rectangle.bottomRight.x * displaySize.width,
+                    height: displaySize.height - rectangle.bottomRight.y * displaySize.height
+                )
+            )
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image, options: [:])
+        try? handler.perform([rectangleDetectionRequest])
+        return corners
     }
 }
