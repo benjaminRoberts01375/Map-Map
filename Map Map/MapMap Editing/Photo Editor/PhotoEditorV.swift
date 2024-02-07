8ac054181a0424817b0950b2e4437b4e6beee8d8
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-07 13:20:46 -0500

    Allow rotating the image

--- Map Map/MapMap Editing/Photo Editor/PhotoEditorV.swift
+++ Map Map/MapMap Editing/Photo Editor/PhotoEditorV.swift
@@ -20,6 +20,8 @@ struct PhotoEditorV: View {
     @Binding var screenSpaceImageSize: CGSize
     /// Dispatch queue for cropping images.
     static let perspectiveQueue = DispatchQueue(label: "com.RobertsHousehold.MapMap.PerspectiveFixer", qos: .userInteractive)
+    /// Track rotation amount.
+    @State var rotation: Angle = .zero
     
     /// Create a photo editor instance.
     /// - Parameters:
@@ -43,14 +45,14 @@ struct PhotoEditorV: View {
                 }
                 Spacer()
                 Button {
-                    print("Rotate left")
+                    withAnimation { rotation += Angle(degrees: -90) }
                 } label: {
                     Image(systemName: "rotate.left")
                         .accessibilityLabel("MapMap counter clockwise 90º")
                         .padding()
                 }
                 Button {
-                    print("Rotate right")
+                    withAnimation { rotation += Angle(degrees: 90) }
                 } label: {
                     Image(systemName: "rotate.right")
                         .accessibilityLabel("MapMap clockwise 90º")
@@ -62,21 +64,28 @@ struct PhotoEditorV: View {
             
             GeometryReader { geo in
                 ZStack(alignment: .center) {
+                    let rotated = rotation.degrees.truncatingRemainder(dividingBy: 180) != .zero
+                    let size = CGSize(
+                        width: rotated ? geo.size.height : geo.size.width,
+                        height: rotated ? geo.size.width : geo.size.height
+                    )
                     MapMapV(mapMap: mapMap, mapType: .original)
                         .onViewResizes { _, update in
                             handleTracker *= update / self.screenSpaceImageSize
                             self.screenSpaceImageSize = update
                         }
                         .frame(
-                            width: geo.size.width * 0.95,
-                            height: geo.size.height * 0.72
+                            width: size.width * 0.95,
+                            height: size.height * 0.72
                         )
+                        .position(CGPoint(size: geo.size / 2))
                     GridOverlayV(corners: $handleTracker)
                         .offset(
                             x: (geo.size.width - screenSpaceImageSize.width) / 2,
                             y: (geo.size.height - screenSpaceImageSize.height) / 2
                         )
                 }
+                .rotationEffect(rotation)
             }
         }
         .task {
