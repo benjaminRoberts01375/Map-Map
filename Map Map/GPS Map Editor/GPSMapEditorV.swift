2c86194a27040f55aa5c55d5c1e18c579427e763
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-09 12:12:45 -0500

    Added base GPSMapEditorV

---
+++ Map Map/GPS Map Editor/GPSMapEditorV.swift
@@ -0,0 +1,43 @@
+//
+//  GPSMapEditorV.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 2/9/24.
+//
+
+import Bottom_Drawer
+import SwiftUI
+
+struct GPSMapEditorV: View {
+    /// The current managed object context.
+    @Environment(\.managedObjectContext) var moc
+    /// Current information about the base map.
+    @Environment(MapDetailsM.self) var mapDetails
+    /// Current name of the GPS Map
+    @State var workingName: String
+    
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                BottomDrawer(verticalDetents: [.content], horizontalDetents: [.center], shortCardSize: 350) { isShortCard in
+                    VStack {
+                        HStack {
+                            TextField("GPS Map Name", text: $workingName)
+                                .padding(.all, 5)
+                                .background(Color.gray.opacity(0.7))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .frame(width: 205)
+                        }
+                        HStack {
+                            Button { moc.reset() } label: { Text("Cancel").bigButton(backgroundColor: .gray) }
+                        }
+                    }
+                    .padding(.bottom, isShortCard ? 0 : 10)
+                }
+                .safeAreaPadding(geo.safeAreaInsets)
+            }
+            .ignoresSafeArea()
+        }
+        .onAppear { mapDetails.preventFollowingUser() }
+    }
+}
