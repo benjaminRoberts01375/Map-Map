57529aaab2daf188bc609b2e0553444c8bbdfa0b
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-09 12:11:20 -0500

    Created GPS Map and GPS Map Coordinate CD entry

---
+++ Map Map/Models/GPSMap+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  GPSMap+CoreDataClass.swift
+//  
+//
+//  Created by Ben Roberts on 2/9/24.
+//
+//
+
+import CoreData
+import Foundation
+
+@objc(GPSMap)
+public class GPSMap: NSManagedObject {
+
+}
