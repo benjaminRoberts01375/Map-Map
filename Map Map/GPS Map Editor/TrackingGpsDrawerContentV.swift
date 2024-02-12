c94fd4e0367ff805199000b82c9d31756222cceb
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-12 14:52:17 -0500

    Rework drawer UI for tracking the user

--- Map Map/GPS Map Editor/TrackingGpsDrawerContentV.swift
+++ Map Map/GPS Map Editor/TrackingGpsDrawerContentV.swift
@@ -13,34 +13,42 @@ struct TrackingGpsDrawerContentV: View {
     /// Fire a timer notification every 0.25 seconds.
     @State var timer = Timer.publish(every: 0.25, on: .main, in: .common).autoconnect()
     /// Seconds since this view has been open.
-    @State var additionalSeconds: TimeInterval = .zero
+    @State var additionalSeconds: Int = .zero
     /// GPS user location.
     @State private var locationsHandler = LocationsHandler.shared
     /// Current information about the base map.
     @Environment(MapDetailsM.self) var mapDetails
+    /// Current speed of the user
+    @State var speed: Measurement<UnitSpeed> = Measurement(value: 0, unit: .metersPerSecond)
     
     var body: some View {
-        VStack {
-            HStack {
-                let totalSeconds: TimeInterval = additionalSeconds + Double(gpsMap.durationSeconds)
-                Text(totalSeconds.description)
-                    .font(.title)
-                    .fontWidth(.condensed)
-                Spacer()
-                Text("Done")
-                    .font(.title)
-            }
-            HStack {
+        HStack {
+            let totalSeconds: TimeInterval = Double(additionalSeconds + Int(gpsMap.durationSeconds))
+            Text(totalSeconds.description)
+                .font(.system(size: 35))
+                .fontWidth(.condensed)
+                .bigButton(backgroundColor: .gray)
+            Spacer()
+            VStack(alignment: .trailing) {
                 Text(LocationDisplayMode.metersToString(meters: Double(gpsMap.distance)))
-                    .foregroundStyle(.secondary)
-                    .fontWidth(.condensed)
-                Spacer()
-                Text("max u ### max d ###")
-                    .foregroundStyle(.secondary)
-                    .fontWidth(.condensed)
+                Text("\(LocationDisplayMode.speedToString(speed: speed))")
+            }
+            .foregroundStyle(.secondary)
+            .fontWidth(.condensed)
+            VStack(alignment: .leading) {
+                Text("max u ###")
+                Text("max d ###")
+            }
+            .foregroundStyle(.secondary)
+            .fontWidth(.condensed)
+            Button {
+                print("Trigger confirmation")
+            } label: {
+                Text("Done")
+                    .font(.title3)
+                    .bigButton(backgroundColor: .red)
             }
         }
-        .padding(.horizontal, 10)
         .onReceive(timer) { _ in
             let startDate: Date
             if let trackingStartDate = gpsMap.trackingStartDate { startDate = trackingStartDate }
@@ -49,10 +57,14 @@ struct TrackingGpsDrawerContentV: View {
                 gpsMap.trackingStartDate = date
                 startDate = date
             }
-            additionalSeconds = Date().timeIntervalSince(startDate)
+            additionalSeconds = Int(Date().timeIntervalSince(startDate))
         }
         .onChange(of: locationsHandler.lastLocation) { _, update in
             _ = gpsMap.addNewCoordinate(clLocation: update)
+        }
+        .onChange(of: additionalSeconds) {
+            let totalSeconds: TimeInterval = Double(additionalSeconds + Int(gpsMap.durationSeconds))
+            self.speed = Measurement(value: Double(gpsMap.distance) / totalSeconds, unit: .metersPerSecond)
         }
         .onAppear { mapDetails.followUser() }
     }
