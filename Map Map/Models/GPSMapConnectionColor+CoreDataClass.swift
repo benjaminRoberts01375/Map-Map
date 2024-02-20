aa3f33f1a540565a00e07a2f3b9fd9f8cccfcd38
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-20 13:32:18 -0500

    Created GPS Connection Color

---
+++ Map Map/Models/GPSMapConnectionColor+CoreDataClass.swift
@@ -0,0 +1,13 @@
+//
+//  GPSMapConnectionColor+CoreDataClass.swift
+//  
+//
+//  Created by Ben Roberts on 2/20/24.
+//
+//
+
+import CoreData
+import Foundation
+
+@objc(GPSMapConnectionColor)
+public class GPSMapConnectionColor: NSManagedObject { }
