f39a4fde7e4a957213468a6bfca6bbe1024ea029
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-19 20:58:18 -0500

    Added init for GPSMapCoordinateConnection

--- Map Map/Models/GPSMapCoordinateConnection+CoreDataClass.swift
+++ Map Map/Models/GPSMapCoordinateConnection+CoreDataClass.swift
@@ -13,4 +13,16 @@ import Foundation
 public class GPSMapCoordinateConnection: NSManagedObject {
     var start: GPSMapCoordinate? { self.coordinates?.array.first as? GPSMapCoordinate }
     var end: GPSMapCoordinate? { self.coordinates?.array.last as? GPSMapCoordinate }
+
+public extension GPSMapCoordinateConnection {
+    /// Create a GPSMapCoordinateConnection from a starting and ending point.
+    /// - Parameters:
+    ///   - start: Starting point.
+    ///   - end: Ending point.
+    ///   - context: Managed Object Context.
+    convenience init(start: GPSMapCoordinate, end: GPSMapCoordinate? = nil, context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.addToCoordinates(start)
+        if let end = end { self.addToCoordinates(end) }
+    }
 }
