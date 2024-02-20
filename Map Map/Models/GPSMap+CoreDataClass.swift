156cf8f7ac5fdeefedd8877c860b7a3360072b30
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-20 11:00:02 -0500

    Improve organization

--- Map Map/Models/GPSMap+CoreDataClass.swift
+++ Map Map/Models/GPSMap+CoreDataClass.swift
@@ -38,30 +38,26 @@ public class GPSMap: NSManagedObject {
         else { return nil }
         let truncAlt = Int16(clLocation.altitude) // Track min/max altitude
         let newCoordinate = GPSMapCoordinate(location: clLocation, moc: moc)
+        
         if let lastConnection = self.unwrappedConnections.last {
             if self.heightMax < truncAlt { self.heightMax = truncAlt }
             else if self.heightMin > truncAlt { self.heightMin = truncAlt }
+            
             if let startCoordinate = lastConnection.end { // Create a new connection using previous end as new start
-                self.addToConnections(GPSMapCoordinateConnection(
-                    start: startCoordinate,
-                    end: newCoordinate,
-                    context: moc
-                ))
+                self.addToConnections(GPSMapCoordinateConnection(start: startCoordinate, end: newCoordinate, context: moc))
                 self.distance += Int32(clLocation.distance(from: startCoordinate.clLocation))
             }
-            else { // Use the existing connection with current as new end
+            else if let startCoordinate = lastConnection.start { // Use the existing connection with current as new end
                 lastConnection.end = newCoordinate
-                if let startCoordinate = lastConnection.start {
-                    self.distance += Int32(clLocation.distance(from: startCoordinate.clLocation))
-                }
+                self.distance += Int32(clLocation.distance(from: startCoordinate.clLocation))
             }
+            return newCoordinate
         }
-        else {
-            self.heightMax = truncAlt
-            self.heightMin = truncAlt
-            // Create new connection with current as start
-            self.addToConnections(GPSMapCoordinateConnection(start: newCoordinate, context: moc))
-        }
+        
+        self.heightMax = truncAlt
+        self.heightMin = truncAlt
+        // Create new connection with current as start
+        self.addToConnections(GPSMapCoordinateConnection(start: newCoordinate, context: moc))
         return newCoordinate
     }
 }
