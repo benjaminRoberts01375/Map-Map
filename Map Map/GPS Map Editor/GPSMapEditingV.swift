aaee59f56d851a7a183063f895a7b9ccf44d9932
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-21 12:25:05 -0500

    Allow for tracking editing mode

--- Map Map/GPS Map Editor/GPSMapEditingV.swift
+++ Map Map/GPS Map Editor/GPSMapEditingV.swift
@@ -11,6 +11,12 @@ struct GPSMapEditingV: View {
     @State var workingName: String
     @ObservedObject var gpsMap: GPSMap
     @Environment(\.managedObjectContext) var moc
+    @State private var mode: EditingMode = .editing
+    
+    private enum EditingMode {
+        case editing
+        case painting
+    }
     
     init(_ gpsMap: GPSMap) {
         self.workingName = gpsMap.name ?? ""
@@ -19,11 +25,23 @@ struct GPSMapEditingV: View {
     
     var body: some View {
         VStack {
-            TextField("GPS Map name", text: $workingName)
-                .padding(.all, 5)
-                .background(Color.gray.opacity(0.7))
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .frame(width: 205)
+            HStack {
+                TextField("GPS Map name", text: $workingName)
+                    .padding(.all, 5)
+                    .background(Color.gray.opacity(0.7))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .frame(width: 205)
+                Button{
+                    self.mode = .painting
+                } label: {
+                    Image(systemName: "paintbrush.pointed.fill")
+                        .accessibilityLabel("Markup MapMap")
+                        .padding(4)
+                        .background(.gray)
+                        .clipShape(Circle())
+                }
+                .buttonStyle(.plain)
+            }
             HStack {
                 Button(
                     action: { gpsMap.unwrappedEditing = .viewing },
