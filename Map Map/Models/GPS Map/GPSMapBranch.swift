b3ea8408e87304bfccfcafef707c711df4fe3266
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-23 16:09:30 -0500

    allow setting of the branch color

--- Map Map/Models/GPS Map/GPSMapBranch.swift
+++ Map Map/Models/GPS Map/GPSMapBranch.swift
@@ -7,13 +7,38 @@
 //
 
 import CoreData
-import Foundation
 import MapKit
+import SwiftUI
 
 @objc(GPSMapBranch)
 public class GPSMapBranch: NSManagedObject {
     var unwrappedConnections: [GPSMapCoordinateConnection] {
         return (self.connections?.array as? [GPSMapCoordinateConnection] ?? []).filter { $0.end != nil }
+    }
+    
+    /// Background color of the Marker.
+    var branchColor: Color {
+        get {
+            if let color = self.color {
+                return Color(red: color.red, green: color.green, blue: color.blue)
+            }
+            else {
+                guard let moc = self.managedObjectContext else { return Color.red }
+                let defaultColor = GPSMapConnectionColor(moc: moc)
+                self.color = defaultColor
+                return Color(red: defaultColor.red, green: defaultColor.green, blue: defaultColor.blue)
+            }
+        }
+        set(newValue) {
+            if let moc = self.managedObjectContext {
+                var red: CGFloat = 0.0
+                var green: CGFloat = 0.0
+                var blue: CGFloat = 0.0
+                var alpha: CGFloat = 0.0
+                _ = UIColor(newValue).getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+                self.color = GPSMapConnectionColor(r: red, g: green, b: blue, moc: moc)
+            }
+        }
     }
     
     public func addNewCoordinate(clLocation: CLLocation) -> GPSMapCoordinate? {
@@ -49,6 +74,6 @@ public class GPSMapBranch: NSManagedObject {
 extension GPSMapBranch {
     convenience init(moc: NSManagedObjectContext) {
         self.init(context: moc)
-        self.color = GPSMapConnectionColor(r: 67, g: 122, b: 234, moc: moc)
+        self.color = GPSMapConnectionColor(moc: moc)
     }
 }
