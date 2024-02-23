e9b8fa856b889b54c1da0ec8b97b235cda4eaa5a
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-22 22:15:25 -0500

    Added branches for GPSMaps

---
+++ Map Map/Models/GPS Map/GPSMapBranch+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  GPSMapBranch+CoreDataClass.swift
+//  
+//
+//  Created by Ben Roberts on 2/22/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(GPSMapBranch)
+public class GPSMapBranch: NSManagedObject {
+
+}
