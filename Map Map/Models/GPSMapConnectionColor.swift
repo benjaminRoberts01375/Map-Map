cf323f03b4395042f6d1264f2af069975b16636d
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-23 01:28:22 -0500

    Created conv. init for GPSMapConnectionColor

--- Map Map/Models/GPSMapConnectionColor.swift
+++ Map Map/Models/GPSMapConnectionColor.swift
@@ -11,3 +11,12 @@ import Foundation
 
 @objc(GPSMapConnectionColor)
 public class GPSMapConnectionColor: NSManagedObject { }
+
+extension GPSMapConnectionColor {
+    convenience init(r red: UInt8, g green: UInt8, b blue: UInt8, moc: NSManagedObjectContext) {
+        self.init(context: moc)
+        self.red = Int16(red)
+        self.green = Int16(green)
+        self.blue = Int16(blue)
+    }
+}
