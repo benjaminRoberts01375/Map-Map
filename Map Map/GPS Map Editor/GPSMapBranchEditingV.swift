fd2c67eb781ff965e618f2775f2dbdc7a76122ee
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-27 21:06:48 -0500

    Initial view for editing branches

--- Map Map/GPS Map Editor/GPSMapBranchEditingV.swift
+++ Map Map/GPS Map Editor/GPSMapBranchEditingV.swift
@@ -5,11 +5,30 @@
 //  Created by Ben Roberts on 2/23/24.
 //
 
+import RangeSlider
 import SwiftUI
 
 struct GPSMapBranchEditingV: View {
     @ObservedObject var gpsMapBranch: GPSMapBranch
-    @State private var workingName: String = ""
+    @State private var workingName: String
+    let rangeIndicies: ClosedRange<Double>
+    @State var selectedRangeIndicies: ClosedRange<Double>
+    @Environment(\.managedObjectContext) var moc
+    let isDefaultBranch: Bool
+    
+    init(gpsMapBranch: GPSMapBranch) {
+        self.gpsMapBranch = gpsMapBranch
+        self.workingName = gpsMapBranch.name ?? ""
+        self.rangeIndicies = 0...Double(gpsMapBranch.gpsMap?.allConnections.count ?? 0)
+        self.selectedRangeIndicies = rangeIndicies
+        self.isDefaultBranch = gpsMapBranch.gpsMap?.unwrappedBranches.first == gpsMapBranch
+        guard let firstConnection = gpsMapBranch.unwrappedConnections.first,
+              let lastConnection = gpsMapBranch.unwrappedConnections.last,
+              let firstIndex = gpsMapBranch.gpsMap?.allConnections.firstIndex(where: { $0 == firstConnection }),
+              let lastIndex = gpsMapBranch.gpsMap?.allConnections.firstIndex(where: { $0 == lastConnection })
+        else { return }
+        self.selectedRangeIndicies = Double(firstIndex)...Double(lastIndex)
+    }
     
     var body: some View {
         VStack {
@@ -22,9 +41,12 @@ struct GPSMapBranchEditingV: View {
                 ColorPicker("", selection: $gpsMapBranch.branchColor, supportsOpacity: false)
                     .labelsHidden()
             }
-            HStack {
-                
+            if !isDefaultBranch {
+                HorizontalRangeSliderV(value: $selectedRangeIndicies, range: rangeIndicies)
             }
+        }
+        .onChange(of: selectedRangeIndicies) { _, newValue in
+            print(newValue)
         }
     }
 }
