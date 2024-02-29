5baad12a99061ac7809722d0402bea3ca7fea59f
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-29 15:21:00 -0500

    Dynamically highlight connections to add to branch

--- Map Map/GPS Map Editor/GPSMapBranchEditingV.swift
+++ Map Map/GPS Map Editor/GPSMapBranchEditingV.swift
@@ -39,10 +39,52 @@ struct GPSMapBranchEditingV: View {
                 ColorPicker("", selection: $gpsMapBranch.branchColor, supportsOpacity: false)
                     .labelsHidden()
             }
+            HorizontalRangeSliderV(value: $selectedRangeIndicies, range: rangeIndicies)
+        }
+        .onChange(of: selectedRangeIndicies) { oldValue, newValue in
+            adjustConnectedBranches(oldIndicies: oldValue, newIndicies: newValue)
+        }
+        .onAppear {
+            guard let gpsMap = gpsMapBranch.gpsMap else { return }
+            _ = gpsMap.unwrappedConnections.map { $0.editing = .editing(gpsMapBranch) }
+        }
+    }
+    
+    func adjustConnectedBranches(oldIndicies: ClosedRange<Double>, newIndicies: ClosedRange<Double>) {
+        guard let connections = gpsMapBranch.gpsMap?.unwrappedConnections // Get all connections
+        else {
+            selectedRangeIndicies = oldIndicies // Reset range
+            return
+        }
+        // If either the selected lower or upper bound is out of bounds, return
+        if selectedRangeIndicies.lowerBound < rangeIndicies.lowerBound ||
+            selectedRangeIndicies.upperBound > rangeIndicies.upperBound {
+            return
+        }
+        
+        // If the old index of lower bound is less than the new one (got slid down)
+        if oldIndicies.lowerBound > newIndicies.lowerBound {
+            for index in Int(newIndicies.lowerBound)..<Int(oldIndicies.lowerBound) {
+                connections[index].editing = .editing(gpsMapBranch)
             }
         }
-        .onChange(of: selectedRangeIndicies) { _, newValue in
-            print(newValue)
+        // If old index of lower bound is greater than new one (got slid up)
+        else if oldIndicies.lowerBound < newIndicies.lowerBound {
+            for index in Int(oldIndicies.lowerBound)..<Int(newIndicies.lowerBound) {
+                connections[index].editing = .standard
+            }
+        }
+        // If the new upper bound is greater than the old upper bound (got slid up)
+        if newIndicies.upperBound > oldIndicies.upperBound {
+            for index in Int(oldIndicies.upperBound)..<Int(newIndicies.upperBound) {
+                connections[index].editing = .editing(gpsMapBranch)
+            }
+        }
+        // If the new upper bound is less than the old upper bound (got slid down)
+        else if newIndicies.upperBound < oldIndicies.upperBound {
+            for index in Int(newIndicies.upperBound)..<Int(oldIndicies.upperBound) {
+                connections[index].editing = .standard
+            }
         }
     }
 }
