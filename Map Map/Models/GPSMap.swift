75fe9520424d93cdd5b6dc4c427df9b29e73f1e6
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-02-28 22:21:22 -0500

    Allow GPSMap to store connections

--- Map Map/Models/GPSMap.swift
+++ Map Map/Models/GPSMap.swift
@@ -18,10 +18,8 @@ public class GPSMap: NSManagedObject {
     }
     
     /// Get an array of all the available connections for this MapMap
-    var allConnections: [GPSMapCoordinateConnection] {
-        var connectionBunches: [[GPSMapCoordinateConnection]] = []
-        for branch in unwrappedBranches { connectionBunches.append(branch.unwrappedConnections) }
-        return connectionBunches.flatMap({ $0 })
+    var unwrappedConnections: [GPSMapCoordinateConnection] {
+        return self.connections?.array as? [GPSMapCoordinateConnection] ?? []
     }
     
     /// Track how this GPS map is being edited.
@@ -45,6 +43,5 @@ public extension GPSMap {
     convenience init(moc: NSManagedObjectContext) {
         self.init(context: moc)
         self.unwrappedEditing = .settingUp
-        self.addToBranches(GPSMapBranch(moc: moc))
     }
 }
