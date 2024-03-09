e34c375dd1e48599ea725862c9232ef225b6b7da
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-09 16:45:50 -0500

    Display unsorted line endings

--- Map Map/Generic View Components/GPS Map/GPSMapV.swift
+++ Map Map/Generic View Components/GPS Map/GPSMapV.swift
@@ -14,11 +14,74 @@ struct GPSMapV: View {
     /// GPS Map to view.
     @ObservedObject var gpsMap: GPSMap
     
+    @State var looseConnections: [[CGPoint]] = []
+    
     var body: some View {
         ZStack {
             ForEach(gpsMap.unwrappedBranches) { branch in
                 GPSMapBranchV(gpsMapBranch: branch)
             }
+            ForEach(looseConnections, id: \.self) { branch in
+                MultiLine(points: branch)
+                    .stroke(style: StrokeStyle(lineWidth: 6, lineCap: .round))
+                    .foregroundStyle(GPSMapConnectionColor.defaultColor)
+            }
         }
+        .onChange(of: mapDetails.mapCamera) { setSSLineNodes() }
+    }
+    
+    /// Update line positions from the line end positions func.
+    func setSSLineNodes() {
+        Task {
+            let lineNodes = await calculateSSLineEndPos()
+            DispatchQueue.main.async {
+                self.looseConnections = lineNodes
+            }
+        }
+    }
+    
+    func calculateSSLineEndPos() async -> [[CGPoint]] {
+        let spanMultiplier = 1.1
+        let correctedSpan = MKCoordinateSpan(
+            latitudeDelta: mapDetails.region.span.latitudeDelta * spanMultiplier,
+            longitudeDelta: mapDetails.region.span.longitudeDelta * spanMultiplier
+        )
+        let ssMapMesh = CGRect(
+            x: mapDetails.region.center.latitude - correctedSpan.latitudeDelta / 2,
+            y: mapDetails.region.center.longitude - correctedSpan.longitudeDelta / 2,
+            width: correctedSpan.latitudeDelta,
+            height: correctedSpan.longitudeDelta
+        )
+        var final: [[CGPoint]] = []
+        var current: [CGPoint] = []
+        var lastCoord: GPSMapCoordinate?
+        
+        for connection in gpsMap.unsortedConnections {
+            if let startCoord = connection.start,
+               let endCoord = connection.end,
+               ssMapMesh.contains(CGPoint(x: startCoord.coordinates.latitude, y: startCoord.coordinates.longitude)) ||
+                ssMapMesh.contains(CGPoint(x: endCoord.coordinates.latitude, y: endCoord.coordinates.longitude)),
+               let endPoint = mapDetails.mapProxy?.convert(endCoord.coordinates, to: .global) {
+                if startCoord != lastCoord {
+                    final.append(current)
+                    current = .init()
+                }
+                current.append(endPoint)
+                lastCoord = endCoord
+            }
+        }
+        final.append(current)
+        return final
+    }
+}
+
+extension CGPoint: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(x)
+        hasher.combine(y)
+    }
+
+    public static func == (lhs: CGPoint, rhs: CGPoint) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
     }
 }
