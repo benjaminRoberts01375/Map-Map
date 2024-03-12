447a8c9b5d001f4e21ea6286f16e36204c976e86
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-12 12:54:29 -0400

    Base widget setup

---
+++ GPSTrackingLA/GPSTrackingLA.swift
@@ -0,0 +1,20 @@
+//
+//  GPSTrackingLA.swift
+//  GPSTrackingLA
+//
+//  Created by Ben Roberts on 3/12/24.
+//
+
+import ActivityKit
+import SwiftUI
+import WidgetKit
+
+struct GPSTrackingLA: Widget { // View controller
+    var body: some WidgetConfiguration {
+        ActivityConfiguration(for: <#T##ActivityAttributes.Protocol#>) { context in
+            VStack { }
+        } dynamicIsland: { context in
+            <#code#>
+        }
+    }
+}
