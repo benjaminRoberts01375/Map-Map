d61d2e7d0be542ca68772cfec917ec5da37e816b
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-14 00:33:31 -0400

    Added GPSTrackingAttributes

---
+++ GPSTrackingLA/GPSTrackingData.swift
@@ -0,0 +1,26 @@
+//
+//  LAState.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 3/12/24.
+//
+
+import ActivityKit
+import CoreLocation
+
+struct GPSTrackingAttributes {
+    // Static values:
+    let gpsMapName: String
+}
+
+extension GPSTrackingAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        // Dynamic values:
+        var userLongitude: Double
+        var userLatitude: Double
+        var seconds: Int32
+        var speed: Measurement<UnitSpeed>
+        var highPoint: Int16
+        var lowPoint: Int16
+    }
+}
