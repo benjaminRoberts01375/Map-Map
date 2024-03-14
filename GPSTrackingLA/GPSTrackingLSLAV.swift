124072297bf33f68585db962e9faf8711f533810
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-14 17:58:52 -0400

    Color and formatting

--- GPSTrackingLA/GPSTrackingLSLAV.swift
+++ GPSTrackingLA/GPSTrackingLSLAV.swift
@@ -5,12 +5,13 @@
 //  Created by Ben Roberts on 3/14/24.
 //
 
-import WidgetKit
 import SwiftUI
+import WidgetKit
 
 struct GPSTrackingLSLAV: View {
+    @Environment(\.colorScheme) var colorScheme
     let context: ActivityViewContext<GPSTrackingAttributes>
-
+    
     var body: some View {
         VStack {
             HStack {
@@ -20,14 +21,22 @@ struct GPSTrackingLSLAV: View {
                     .bigButton(backgroundColor: .gray, minWidth: 120)
                 Spacer(minLength: 0)
             }
+            .padding()
             HStack {
+                Spacer()
                 Text(LocationDisplayMode.metersToString(meters: Double(context.state.distance)))
+                Spacer()
                 Text("\(LocationDisplayMode.speedToString(speed: context.state.speed))")
+                Spacer()
                 Text("\(LocationDisplayMode.metersToString(meters: Double(context.state.highPoint))) ↑")
+                Spacer()
                 Text("\(LocationDisplayMode.metersToString(meters: Double(context.state.lowPoint))) ↓")
+                Spacer()
             }
             .foregroundStyle(.secondary)
             .fontWidth(.condensed)
+            .background(colorScheme == .dark ? .black : .white)
         }
+        .background(colorScheme == .dark ? Color(red: 0.33, green: 0.45, blue: 0.28) : Color(red: 0.65, green: 0.82, blue: 0.48))
     }
 }
