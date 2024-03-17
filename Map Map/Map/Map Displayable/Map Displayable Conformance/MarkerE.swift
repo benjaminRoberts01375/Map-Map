502e1e3208f4c4743b1c6c235b46f03b7d2769f7
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-17 14:02:31 -0400

    Conform Marker to MapDisplayable

---
+++ Map Map/Map/Map Displayable/Map Displayable Conformance/MarkerE.swift
@@ -0,0 +1,16 @@
+//
+//  MarkerE.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 3/17/24.
+//
+
+import MapKit
+
+extension Marker: MapDisplayable {
+    var coordinate: CLLocationCoordinate2D { self.coordinates }
+    
+    var cameraAlignment: MapCameraAlignment { .distance(6000) }
+    
+    var heading: Double { -(self.lockRotationAngleDouble ?? 0) }
+}
