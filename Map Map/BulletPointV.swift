d7522b3c9ad4980272021a042254e2d8202c47dd
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-22 22:04:25 -0400

    Created dedicated StoreV BulletPointV extension file

---
+++ Map Map/BulletPointV.swift
@@ -0,0 +1,38 @@
+//
+//  BulletPointV.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 3/22/24.
+//
+
+import SwiftUI
+
+extension StoreV {
+    struct BulletPointV: View {
+        let icon: String
+        let color: Color
+        let title: String
+        let description: String
+        private let size: CGFloat = 35
+        
+        var body: some View {
+            HStack(alignment: .center) {
+                Image(systemName: icon)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: size, height: size)
+                    .padding(.horizontal)
+                    .foregroundStyle(color)
+                    .accessibilityHidden(true)
+                VStack(alignment: .leading) {
+                    Text(title)
+                        .bold()
+                    Text(description)
+                        .foregroundStyle(.secondary)
+                }
+                Spacer()
+            }
+        }
+    }
+
+}
--- Map Map/StoreV.swift
+++ Map Map/StoreV.swift
@@ -185,30 +185,3 @@ fileprivate struct MapMapExplorerTitleV: View {
         }
     }
 }
-
-fileprivate struct BulletPointV: View {
-    let icon: String
-    let color: Color
-    let title: String
-    let description: String
-    private let size: CGFloat = 35
-    
-    var body: some View {
-        HStack(alignment: .center) {
-            Image(systemName: icon)
-                .resizable()
-                .scaledToFit()
-                .frame(width: size, height: size)
-                .padding(.horizontal)
-                .foregroundStyle(color)
-                .accessibilityHidden(true)
-            VStack(alignment: .leading) {
-                Text(title)
-                    .bold()
-                Text(description)
-                    .foregroundStyle(.secondary)
-            }
-            Spacer()
-        }
-    }
-}
