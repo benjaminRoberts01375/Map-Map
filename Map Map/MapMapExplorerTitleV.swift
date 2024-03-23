31eb8461c39fa614ed4ba8f51dde4dbfadb2f6b3
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-22 22:05:27 -0400

    Created dedicated StoreV MapMapExplorerTitleV extension file

---
+++ Map Map/MapMapExplorerTitleV.swift
@@ -0,0 +1,30 @@
+//
+//  MapMapExplorerTitleV.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 3/22/24.
+//
+
+import SwiftUI
+
+extension StoreV {
+    struct MapMapExplorerTitleV: View {
+        var body: some View {
+            VStack {
+                Image(uiImage: UIImage(named: "AppIcon") ?? UIImage())
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .accessibilityHidden(true)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .shadow(radius: 10)
+                Text("Map Map")
+                    .font(.system(size: 70).bold())
+                Text("- Explorer -")
+                    .font(.system(size: 50).bold())
+                    .scaleEffect(x: 0.9)
+                
+            }
+        }
+    }
+}
--- Map Map/StoreV.swift
+++ Map Map/StoreV.swift
@@ -165,23 +165,3 @@ struct StoreV: View {
     Color.clear
         .sheet(isPresented: .constant(true)) { StoreV(purchased: .constant(true)) }
 }
-
-fileprivate struct MapMapExplorerTitleV: View {
-    var body: some View {
-        VStack {
-            Image(uiImage: UIImage(named: "AppIcon") ?? UIImage())
-                .resizable()
-                .scaledToFit()
-                .frame(width: 150, height: 150)
-                .accessibilityHidden(true)
-                .clipShape(RoundedRectangle(cornerRadius: 25))
-                .shadow(radius: 10)
-            Text("Map Map")
-                .font(.system(size: 70).bold())
-            Text("- Explorer -")
-                .font(.system(size: 50).bold())
-                .scaleEffect(x: 0.9)
-
-        }
-    }
-}
