7deea7f74515fb73ae00b4cc978c6c5390bcbb63
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-22 21:46:50 -0400

    Fixed access to AddMapMenuV

--- Map Map/Default Drawer Header/AddMapMenuV.swift
+++ Map Map/Default Drawer Header/AddMapMenuV.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 extension DefaultDrawerHeaderV {
-    private struct AddMapMenuV: View {
+    struct AddMapMenuV: View {
         /// Current Core Data managed object context.
         @Environment(\.managedObjectContext) private var moc
         
