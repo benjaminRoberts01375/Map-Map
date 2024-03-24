fa72efa10348e11a7592a9cd48bc470a2c29394b
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-24 12:54:51 -0400

    Create MeasurementsVM

---
+++ Map Map/Model Views/MeasurementsVM.swift
@@ -0,0 +1,15 @@
+//
+//  MeasurementsVM.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 3/24/24.
+//
+
+import SwiftUI
+
+extension MeasurementsV {
+    @Observable
+    final class MeasurementsVM {
+        
+    }
+}
