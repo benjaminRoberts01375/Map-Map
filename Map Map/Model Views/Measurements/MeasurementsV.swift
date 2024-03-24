da7100273770871e080f17889529624a2d2205e0
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-24 14:54:16 -0400

    Render Measurements to screen

--- Map Map/Model Views/Measurements/MeasurementsV.swift
+++ Map Map/Model Views/Measurements/MeasurementsV.swift
@@ -13,6 +13,37 @@ struct MeasurementsV: View {
     @State var viewModel: ViewModel = ViewModel()
     
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        ForEach(Array(viewModel.ssClusters.enumerated()), id: \.offset) { _, cluster in
+            ForEach(cluster) { edge in
+                ZStack {
+                    Line(startingPos: CGSize(cgPoint: edge.startPoint), endingPos: CGSize(cgPoint: edge.endPoint)) // Outline line
+                        .stroke(style: StrokeStyle(lineWidth: 6, lineCap: .round))
+                        .foregroundStyle(.black.opacity(0.5))
+                    Line(startingPos: CGSize(cgPoint: edge.startPoint), endingPos: CGSize(cgPoint: edge.endPoint)) // Foreground line
+                        .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                        .lineLabel(
+                            startingPos: edge.startPoint,
+                            endingPos: edge.endPoint,
+                            distance: edge.distance
+                        )
+                }
+            }
+        }
+        .onChange(of: measurementCoordinates.count) {
+            Task {
+                let edges = determineEdges()
+                let ssEdges = determineSSConnectionPoints()
+                await MainActor.run {
+                    self.viewModel.clusters = edges
+                    self.viewModel.ssClusters = ssEdges
+                }
+            }
+        }
+        .onChange(of: mapDetails.mapCamera) {
+            Task {
+                let ssEdges = determineSSConnectionPoints()
+                await MainActor.run { self.viewModel.ssClusters = ssEdges }
+            }
+        }
     }
 }
