825411cc371c55cc777294dc36c8577ea192b125
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-24 16:15:59 -0400

    Base gesture for moving and creating points

--- Map Map/Measurement Editor/MeasurementEditorV.swift
+++ Map Map/Measurement Editor/MeasurementEditorV.swift
@@ -13,11 +13,21 @@ struct MeasurementEditorV: View {
     /// Editor being used by Map Map.
     @State var viewModel: ViewModel
     
+    @Environment(MapDetailsM.self) var mapDetails
+    
+    @Environment(\.managedObjectContext) var moc
+    
     var body: some View {
         ZStack {
             Color.black
                 .opacity(0.5)
                 .ignoresSafeArea()
+                .onTapGesture {
+                    viewModel.selectedCoordinateEnd = nil
+                    viewModel.selectedCoordinateStart = nil
+                }
+                .accessibilityAddTraits(.isButton)
+                .gesture(newMeasurement)
         }
     }
 }
