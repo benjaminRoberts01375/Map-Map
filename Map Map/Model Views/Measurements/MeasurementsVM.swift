2e1c2f494905971d121e3fb427926fa7d3ee3ba7
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-24 13:08:03 -0400

    Determine which points are considered "on screen"

--- Map Map/Model Views/Measurements/MeasurementsVM.swift
+++ Map Map/Model Views/Measurements/MeasurementsVM.swift
@@ -5,15 +5,32 @@
 //  Created by Ben Roberts on 3/24/24.
 //
 
+import MapKit
 import SwiftUI
 
 extension MeasurementsV {
     @Observable
-        
     final class ViewModel {
+        var measurementPoints: [MapMeasurementCoordinate : CGPoint] = [:]
     }
     
     func generateOnScreenPoints() {
-        
+        var measurementPoints: [MapMeasurementCoordinate : CGPoint] = [:]
+        let spanMultiplier = 2.0
+        let correctedSpan = MKCoordinateSpan(
+            latitudeDelta: mapDetails.region.span.latitudeDelta * spanMultiplier,
+            longitudeDelta: mapDetails.region.span.longitudeDelta * spanMultiplier
+        )
+        let ssMapMesh = CGRect(
+            x: mapDetails.region.center.latitude - correctedSpan.latitudeDelta / 2,
+            y: mapDetails.region.center.longitude - correctedSpan.longitudeDelta / 2,
+            width: correctedSpan.latitudeDelta,
+            height: correctedSpan.longitudeDelta
+        )
+        for coord in measurementCoords {
+            guard let position = mapDetails.mapProxy?.convert(coord.coordinates, to: .global) else { continue }
+            if ssMapMesh.contains(position) { measurementPoints[coord] = position }
+        }
+        viewModel.measurementPoints = measurementPoints
     }
 }
