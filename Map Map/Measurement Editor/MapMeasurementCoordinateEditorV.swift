4a0956b62df0899822fee24af6ba8cc976747989
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-24 18:43:33 -0400

    Removed unused geometry reader

--- Map Map/Measurement Editor/MapMeasurementCoordinateEditorV.swift
+++ Map Map/Measurement Editor/MapMeasurementCoordinateEditorV.swift
@@ -39,7 +39,7 @@ struct MapMeasurementCoordinateEditorV: View {
             .onChanged { update in
                 if !isDragging {
                     if let selectedMeasurement = selectedMeasurement,
-                        let measurementPos = mapDetails.mapProxy?.convert(selectedMeasurement.coordinates, to: .global) {
+                       let measurementPos = mapDetails.mapProxy?.convert(selectedMeasurement.coordinates, to: .global) {
                         startingPos = CGSize(cgPoint: measurementPos)
                     }
                     else { startingPos = CGSize(cgPoint: update.startLocation) }
@@ -97,71 +97,69 @@ struct MapMeasurementCoordinateEditorV: View {
     }
     
     var body: some View {
-        GeometryReader { _ in
-            ZStack {
-                Color.black
-                    .opacity(0.5)
-                    .ignoresSafeArea()
-                    .onTapGesture { selectedMeasurement = nil }
-                    .accessibilityAddTraits(.isButton)
-                    .gesture(drawGesture)
-                if endingPos != startingPos {
-                    ZStack {
-                        Line(startingPos: startingPos, endingPos: endingPos)
-                            .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round))
-                            .shadow(radius: 2)
-                            .lineLabel(startingPos: CGPoint(size: startingPos), endingPos: CGPoint(size: endingPos), distance: distance)
-                        if selectedMeasurement == nil { HandleV(position: $startingPos) }
-                        HandleV(position: $endingPos)
-                    }
-                    .ignoresSafeArea()
-                }
-                
-                ForEach(measurements) { measurement in
-                    Button {
-                        selectedMeasurement = measurement
-                    } label: {
-                        HandleV(
-                            position: handlePositionBinding(for: measurement), 
-                            color: selectedMeasurement == measurement ? Color.highlightBlue : HandleV.defaultColor,
-                            deferPosition: true
-                        )
-                    }
-                    .position(
-                        x: handlePositions[measurement]?.width ?? .zero,
-                        y: handlePositions[measurement]?.height ?? .zero
-                    )
+        ZStack {
+            Color.black
+                .opacity(0.5)
+                .ignoresSafeArea()
+                .onTapGesture { selectedMeasurement = nil }
+                .accessibilityAddTraits(.isButton)
+                .gesture(drawGesture)
+            if endingPos != startingPos {
+                ZStack {
+                    Line(startingPos: startingPos, endingPos: endingPos)
+                        .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                        .shadow(radius: 2)
+                        .lineLabel(startingPos: CGPoint(size: startingPos), endingPos: CGPoint(size: endingPos), distance: distance)
+                    if selectedMeasurement == nil { HandleV(position: $startingPos) }
+                    HandleV(position: $endingPos)
                 }
                 .ignoresSafeArea()
-                
-                BottomDrawer(verticalDetents: [.content], horizontalDetents: [.center]) { _ in
-                    VStack {
-                        Text("Drag to measure.")
-                            .foregroundStyle(.secondary)
-                        HStack {
-                            // Done button
-                            Button {
-                                editing = .nothing
-                                cleanupMeasurements()
-                                try? moc.save()
-                            } label: {
-                                Text("Done")
-                                    .bigButton(backgroundColor: .blue)
-                            }
-                            
-                            // Delete button
-                            Button( action: {
-                                guard let selectedMeasurement = selectedMeasurement
-                                else { return }
-                                moc.delete(selectedMeasurement)
-                                self.selectedMeasurement = nil
-                                try? moc.save()
-                            }, label: {
-                                Text("Delete")
-                                    .bigButton(backgroundColor: .red.opacity(selectedMeasurement != nil ? 1 : 0.5))
-                            })
-                            .disabled(selectedMeasurement == nil)
+            }
+            
+            ForEach(measurements) { measurement in
+                Button {
+                    selectedMeasurement = measurement
+                } label: {
+                    HandleV(
+                        position: handlePositionBinding(for: measurement),
+                        color: selectedMeasurement == measurement ? Color.highlightBlue : HandleV.defaultColor,
+                        deferPosition: true
+                    )
+                }
+                .position(
+                    x: handlePositions[measurement]?.width ?? .zero,
+                    y: handlePositions[measurement]?.height ?? .zero
+                )
+            }
+            .ignoresSafeArea()
+            
+            BottomDrawer(verticalDetents: [.content], horizontalDetents: [.center]) { _ in
+                VStack {
+                    Text("Drag to measure.")
+                        .foregroundStyle(.secondary)
+                    HStack {
+                        // Done button
+                        Button {
+                            editing = .nothing
+                            cleanupMeasurements()
+                            try? moc.save()
+                        } label: {
+                            Text("Done")
+                                .bigButton(backgroundColor: .blue)
                         }
+                        
+                        // Delete button
+                        Button( action: {
+                            guard let selectedMeasurement = selectedMeasurement
+                            else { return }
+                            moc.delete(selectedMeasurement)
+                            self.selectedMeasurement = nil
+                            try? moc.save()
+                        }, label: {
+                            Text("Delete")
+                                .bigButton(backgroundColor: .red.opacity(selectedMeasurement != nil ? 1 : 0.5))
+                        })
+                        .disabled(selectedMeasurement == nil)
                     }
                 }
             }
