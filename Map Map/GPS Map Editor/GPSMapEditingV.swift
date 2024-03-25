057225e904033d1df2b0a4a079e37f49cf58e266
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-03-25 14:10:24 -0400

    Save GPS Map name

--- Map Map/GPS Map Editor/GPSMapEditingV.swift
+++ Map Map/GPS Map Editor/GPSMapEditingV.swift
@@ -45,6 +45,7 @@ struct GPSMapEditingV: View {
             HStack {
                 Button {
                     try? moc.save()
+                    gpsMap.name = workingName
                     gpsMap.isEditing = false
                 } label: {
                     Text("Done").bigButton(backgroundColor: .blue)
