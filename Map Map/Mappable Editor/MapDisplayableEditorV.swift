f6e571c481cee8406af989e150451cb7d97b37b8
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2024-04-29 14:56:43 -0400

    Add MapDisplayableEditorV

---
+++ Map Map/Mappable Editor/MapDisplayableEditorV.swift
@@ -0,0 +1,16 @@
+//
+//  MappableEditor.swift
+//  Map Map
+//
+//  Created by Ben Roberts on 4/29/24.
+//
+
+import SwiftUI
+
+struct MapDisplayableEditorV: View {
+    var editing: MapDisplayable & EditableDataBlock
+    
+    var body: some View {
+        EmptyView()
+    }
+}
